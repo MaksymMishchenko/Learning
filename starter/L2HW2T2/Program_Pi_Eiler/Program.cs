6f7dcdf05e67881be6104a32b6a9827fb78c4286
velmin <webseo89@gmail.com>
2020-05-18 18:37:12 +0300

    Program pi_e

--- starter/L2HW2T2/Program_Pi_Eiler/Program.cs
+++ starter/L2HW2T2/Program_Pi_Eiler/Program.cs
@@ -6,7 +6,13 @@ namespace Program_Pi_Eiler
     {
         static void Main(string[] args)
         {
-            
+            double pi, e;
+            pi = 3.141592653d;
+            e = 2.7182818284590452d;
+
+            Console.WriteLine(pi);
+            Console.WriteLine(e);
+            Console.ReadKey();
         }
     }
 }
