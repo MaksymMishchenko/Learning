241878af7ed8bc3d506fe6e0ee02f5c4e3985d7d
velmin <webseo89@gmail.com>
2020-05-18 15:46:10 +0300

    Lesson two dz step-by-step process

---
+++ starter/L2HW2T1/MachineMathematicsType/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace MachineMathematicsType
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("сюда можна вписать текст", "Type: unsigned char");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("сюда можна вписать текст","Type: char");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("сюда можна вписать текст", "Type: unsigned short");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("сюда можна вписать текст", "Type: short");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("сюда можна вписать текст", "Type: unsigned int");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("сюда можна вписать текст", "Type: int");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("сюда можна вписать текст", "Type: unsigned long int");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("сюда можна вписать текст", "Type: long int");
+        }
+    }
+}
