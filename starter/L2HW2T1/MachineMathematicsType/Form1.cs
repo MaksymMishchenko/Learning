d844475c0b7e0a79f30f9b72991d635e7a53fdf1
velmin <webseo89@gmail.com>
2020-05-18 16:32:33 +0300

    lesson2 HW Add Task - done!

--- starter/L2HW2T1/MachineMathematicsType/Form1.cs
+++ starter/L2HW2T1/MachineMathematicsType/Form1.cs
@@ -13,42 +13,42 @@ namespace MachineMathematicsType
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("сюда можна вписать текст", "Type: unsigned char");
+            MessageBox.Show("0...255", "Тип: целое без знака");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("сюда можна вписать текст","Type: char");
+            MessageBox.Show("-128…+127", "Тип: целое со знаком");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("сюда можна вписать текст", "Type: unsigned short");
+            MessageBox.Show("0...65535", "Тип: целое без знака");
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("сюда можна вписать текст", "Type: short");
+            MessageBox.Show("-32768…+32767", "Тип: целое со знаком");
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("сюда можна вписать текст", "Type: unsigned int");
+            MessageBox.Show("0u...4294967295U", "Тип: целое без знака");
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("сюда можна вписать текст", "Type: int");
+            MessageBox.Show("-2147483648...+2147483647", "Тип: целое со знаком");
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("сюда можна вписать текст", "Type: unsigned long int");
+            MessageBox.Show("0ul...18446744073709551615UL","Тип: целое без знака");
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("сюда можна вписать текст", "Type: long int");
+            MessageBox.Show("-9223372036854775808l...+9223372036854775807l", "Тип: целое со знаком");
         }
     }
 }
