f16ee1051292035602ecf2db10d202d549f869ee
velmin <webseo89@gmail.com>
2020-05-19 23:49:11 +0300

    Create MathOperProgram.V1

--- starter/L3HW3ADDT/ArithOperProgram/Program.cs
+++ starter/L3HW3ADDT/ArithOperProgram/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace ArithOperProgram
+﻿using System;
+
+namespace ArithOperProgram
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // it's very ease task:)
         {
+            int a = 10; 
+            int b = 2;
+
+            int resultSum = a + b;   //создаем переменную resultSum и присваиваем ей результат сложения переменных.
+
+            Console.Write("Результат сложения переменнных: 10 + 2 = ");
+            Console.WriteLine(resultSum);
+
+            int resultSub = a - b;  //создаем переменную resultSub и присваиваем ей результат вычитания переменных.
+
+            Console.Write("Результат вычитания переменнных: 10 - 2 = ");
+            Console.WriteLine(resultSub);
+
+            int resultMult = a * b; //создаем переменную resultMult и присваиваем ей результат умножения переменных.
+
+            Console.Write("Результат умножения переменнных: 10 * 2 = ");
+            Console.WriteLine(resultMult);
+
+            int resultDivis = a / b; //создаем переменную resultDivis и присваиваем ей результат деления переменных.
+
+            Console.Write("Результат деления переменнных: 10 / 2 = ");
+            Console.WriteLine(resultDivis);
+
+             //создаем переменную remainder и присваиваем ей результат получения остатка от числа переменных.
+            int resultRemNam = a % b;
+            int remainder = resultRemNam;
+
+            Console.Write("Результат получения остатка от числа переменных: 10 % 2 = ");
+            Console.WriteLine(remainder);
+
+            Console.ReadKey(); // Delay
         }
+        
     }
 }
--- starter/L3HW3ADDT/ArithOperProgram/ReadMe.txt
+++ starter/L3HW3ADDT/ArithOperProgram/ReadMe.txt
@@ -4,7 +4,7 @@ Create two integer variables and display the results of all arithmetic operation
 
 Solution
 1. create two variables a & b with type integer.
-2. we carry out 5 arithmetic operations: addition, subtraction, multiplication and division, obtaining the remainder of division.
+2. we carry out 5 arithmetic operations: addition, subtraction, multiplication, division, dividend divisor.
 3. display results on screen.
 4. fix the results on the screen.
 
