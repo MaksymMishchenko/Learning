bd1a370507f10807443b8f19552737cbffaaa7ba
velmin <webseo89@gmail.com>
2020-05-20 22:15:50 +0300

    RadiusCircleProgram

--- starter/L3HW3T3/AreaCircleProgram/Program.cs
+++ starter/L3HW3T3/AreaCircleProgram/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace AreaCircleProgram
+﻿using System;
+
+namespace AreaCircleProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
+            const double pi = 3.141; //число Pi
+            var r = pi * Math.Pow(pi, 2); //создаем переменную r, помещаем в нее результат формулы
+            Console.Write("Площадь нашего круга: ");
+            Console.WriteLine(r); //выводим переменную на экран
+            Console.ReadKey(); //Задержка
+
         }
     }
 }
