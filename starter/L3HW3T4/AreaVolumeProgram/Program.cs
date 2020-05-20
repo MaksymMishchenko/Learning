33f2c11a09da51c88524fdc9d59e3c0808993cec
velmin <webseo89@gmail.com>
2020-05-20 23:08:46 +0300

    AreaVolumeProgram

--- starter/L3HW3T4/AreaVolumeProgram/Program.cs
+++ starter/L3HW3T4/AreaVolumeProgram/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace AreaVolumeProgram
+﻿using System;
+
+namespace AreaVolumeProgram
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main() //Объем V цилиндра радиусом – R и высотой – h, вычисляется по формуле: V = πR 2 h 
         {
+            double R = 5, h = 4, pi = 3.141;
+            var V = pi * Math.Pow(R, 2) * h;
+            
+            Console.WriteLine("Объем цилиндра равен: {0}", V);
+            Console.ReadKey();
+            
 
         }
     }
