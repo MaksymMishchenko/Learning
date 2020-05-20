634d23c518407de44a83b7323a84baf87003360b
velmin <webseo89@gmail.com>
2020-05-20 18:18:25 +0300

    program Average

--- starter/L3HW3T2/ConsoleApp1/Program.cs
+++ starter/L3HW3T2/ConsoleApp1/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace ConsoleApp1
+﻿using System;
+
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int a = 8, b = 3, c = 12;
+            float result;
+            result = (a + b + c) / 3; 
+            Console.Write("Среднее арифметическое чисел a, b и c = ");
+            Console.WriteLine(result); //потеря точности, выбрал тип данных Float
+            Console.ReadKey();
+
         }
     }
 }
