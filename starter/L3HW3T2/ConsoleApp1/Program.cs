498fe33bcbc073ae938b3a03dc6b50aae6d7ccb9
velmin <webseo89@gmail.com>
2020-05-20 17:54:27 +0300

    empty project L3HW3T2

--- starter/L3HW3T2/ConsoleApp1/Program.cs
+++ starter/L3HW3T2/ConsoleApp1/Program.cs
@@ -1,12 +1,9 @@
-﻿namespace ComparisonApp1
+﻿namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int x = 10, y = 12, z = 12;
-
-           int 
         }
     }
 }
