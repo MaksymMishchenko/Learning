40efc487ac99ae46f180e0b24272557e923d0fcd
velmin <webseo89@gmail.com>
2020-05-21 18:01:42 +0300

    BugFixAreaVolumeProgram

--- starter/L3HW3T4/AreaVolumeProgram/Program.cs
+++ starter/L3HW3T4/AreaVolumeProgram/Program.cs
@@ -5,17 +5,15 @@ namespace AreaVolumeProgram
     {
         static void Main() //Объем V цилиндра радиусом – R и высотой – h, вычисляется по формуле: V = πR 2 h 
         {
-            double R = 5, h = 4, pi = 3.141;
+            double R = 15, h = 14, pi = 3.141;
             var V = pi * Math.Pow(R, 2) * h;
             
             Console.WriteLine("Объем цилиндра равен: {0}", V);
 
             {
-                double Pi = 3.141, r = 5, H = 4; //Площадь S поверхности цилиндра вычисляется по формуле: S = 2πR2 + 2πR 2 = 2πR(R+h)
-                double a = Pi * Math.Pow(r, 2) * 4;
-                double b = r + H;
-                var S = a = b;
-
+                double Pi = 3.141, r = 15, H = 14; //Площадь S поверхности цилиндра вычисляется по формуле: S = 2πR2 + 2πR 2 = 2πR(R+h)
+                var S  = (2 * Pi * r *(r+H));
+                
                 Console.WriteLine("Площадь поверхности цилиндра: {0} ", S);
             }
                 Console.ReadKey();
