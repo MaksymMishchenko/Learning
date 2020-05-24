c926d8d7fb072c2d28f5f91ba252fd00a6bd8cea
velmin <webseo89@gmail.com>
2020-05-24 23:02:15 +0300

    TranslateProgram

--- starter/L4HW4ADDT/TranslateProgram/Program.cs
+++ starter/L4HW4ADDT/TranslateProgram/Program.cs
@@ -1,9 +1,73 @@
-﻿namespace TranslateProgram
+﻿using System;
+namespace TranslateProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите фрукт на русском языке:");
+
+            string word = Console.ReadLine();
+
+            switch (word)
+            {
+                case "яблоко":
+                {
+                        Console.WriteLine("Ответ перевода: Apple");
+                    break;
+                }
+                case "вишня":
+                {
+                        Console.WriteLine("Ответ: Cherry");
+                        break;
+                }
+                case "апельсин":
+                {
+                    Console.WriteLine("Ответ: Orange");
+                    break;
+                }
+                case "банан":
+                {
+                    Console.WriteLine("Ответ: Bananas");
+                    break;
+                }
+                case "черешня":
+                {
+                    Console.WriteLine("Ответ: Cherries");
+                    break;
+                }
+                case "арбуз":
+                {
+                    Console.WriteLine("Ответ: Watermelon");
+                    break;
+                }
+                case "груша":
+                {
+                    Console.WriteLine("Ответ: Pear");
+                    break;
+                }
+                case "слива":
+                {
+                    Console.WriteLine("Ответ: Plum");
+                    break;
+                }
+                case "клубника":
+                {
+                    Console.WriteLine("Ответ: Strawberry");
+                    break;
+                }
+                case "грейпфрут":
+                {
+                    Console.WriteLine("Ответ: Grapefruit");
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("В переводчике нету слова для перевода. Попробуйте ввести другое слово!");
+                    break;
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
