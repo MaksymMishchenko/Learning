269d45b135d00e5c04c20a485346c385dc9d46fb
velmin <webseo89@gmail.com>
2020-05-28 09:06:30 +0300

    PowerOfTwoProgram

--- starter/L5HW5ADDT/PowerTwoProgram/Program.cs
+++ starter/L5HW5ADDT/PowerTwoProgram/Program.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace PowerTwoProgram
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (Step(a)) Console.WriteLine("The number - {0} is a power of two!", a);
+            else Console.WriteLine("The number - {0} is not a power of two!", a);
+            Console.ReadKey();
+        }
+        public static bool Step(int a)
+        {
+            if (a == 2) return true;
+            else if (a % 2 == 0) return Step(a / 2);
+            else return false;
         }
     }
 }
--- starter/L5HW5ADDT/PowerTwoProgram/ReadMe.txt
+++ starter/L5HW5ADDT/PowerTwoProgram/ReadMe.txt
@@ -2,3 +2,8 @@ Task 1
 It is known that for numbers that are powers of two, only one bit has a value of 1.
 Using Visual Studio, create a project using the ConsoleApplication template.
 Write a program that will check whether the indicated number is a power of two or not.
+
+
+Solution:
+
+We create a check whether the number entered by the user is a power of two. If the condition is met, a message is displayed, which is. If the condition is not met, then we display a message that is not.
