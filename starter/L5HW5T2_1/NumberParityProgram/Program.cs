136d8d458bac8672e8c803b792a0065285a771b0
velmin <webseo89@gmail.com>
2020-05-28 11:46:11 +0300

    NumberParityProgram

--- starter/L5HW5T2_1/NumberParityProgram/Program.cs
+++ starter/L5HW5T2_1/NumberParityProgram/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace NumberParityProgram
+﻿using System;
+
+namespace NumberParityProgram
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main()
+        {   
+            //first option
+            Console.Write("Enter your number:");
+            var value = Convert.ToInt32(Console.ReadLine());
+            if ((value % 2) == 0)
+            {
+                Console.WriteLine("You entered {0}, it's even number", value);
+            }
+
+            else
+
+            {
+                Console.WriteLine("You entered {0}, it's not even number", value);
+            }
+            //second option
+            
+            if ((value % 1) == 0)
+            {
+                Console.WriteLine("You entered {0}, it's even number", value);
+            }
+
+            else
+
+            {
+                Console.WriteLine("You entered {0}, it's not even number", value);
+            }
+
+            Console.ReadKey();
         }
     }
 }
