393b8d3bd3e31dffb667646237cb527abdafc56f
velmin <webseo89@gmail.com>
2020-05-28 09:32:38 +0300

    ExampleDeMorganProgram

--- starter/L5HW5T2/DeMorganProgram/Program.cs
+++ starter/L5HW5T2/DeMorganProgram/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace DeMorganProgram
+﻿using System;
+
+namespace DeMorganProgram
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            bool A = false;
+            bool B = true;
+            //condition before applying de Morgan's theorem
+            if (A | B)
+            {
+                Console.WriteLine("(A | B) = {0}", (A | B));
+            }
+            //condition after applying de Morgan's theorem
+            if (!(!A & !B))
+            {
+                Console.WriteLine("!(!A & !B) = {0}", !(!A & !B));
+            }
+
+            Console.ReadKey();
         }
     }
 }
--- starter/L5HW5T2/DeMorganProgram/ReadMe.txt
+++ starter/L5HW5T2/DeMorganProgram/ReadMe.txt
@@ -2,3 +2,5 @@ Task 2
 Using Visual Studio, create a project using the Console Application template.
 Using the De Morgan theorem, transform the original expression A | B, in an equivalent expression.
 
+Solution:
+In the first example, a logical expression of disjunction. In the second example, the expression of a disjunction using the De Morgan theorem.
