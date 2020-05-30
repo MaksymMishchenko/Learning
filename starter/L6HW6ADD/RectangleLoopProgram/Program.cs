293ef442b5443f4f3810da0d495d7c4dea91cf95
velmin <webseo89@gmail.com>
2020-05-30 22:13:19 +0300

    RectangleLoopProgram

--- starter/L6HW6ADD/RectangleLoopProgram/Program.cs
+++ starter/L6HW6ADD/RectangleLoopProgram/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace RectangleLoopProgram
+﻿using System;
+
+namespace RectangleLoopProgram
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main()
+        {// create loop for
+            for (int vertical = 0; vertical < 9; vertical++)
+            {//create nested loop for on body first loop for 
+                for (int horizont = 0; horizont < 25; horizont++)
+                {
+                    //display on screen
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
