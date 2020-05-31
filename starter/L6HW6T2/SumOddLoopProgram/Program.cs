2e8f4c6f676c8ef39b52993859f8d08ab0cb4a65
velmin <webseo89@gmail.com>
2020-05-31 15:52:38 +0300

    SumLoopProgram

--- starter/L6HW6T2/SumOddLoopProgram/Program.cs
+++ starter/L6HW6T2/SumOddLoopProgram/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace SumOddLoopProgram
+﻿using System;
+
+namespace SumOddLoopProgram
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Input first number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input second number:");
+            int b = Convert.ToInt32(Console.ReadLine()); 
+
+            int sum = 0;
+
+            for (int i = a + 1; i < b; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("The sum of all numbers located between the indicated numbers - {0}", sum);
+            Console.ReadKey();
         }
     }
 }
--- starter/L6HW6T2/SumOddLoopProgram/ReadMe.txt
+++ starter/L6HW6T2/SumOddLoopProgram/ReadMe.txt
@@ -2,3 +2,8 @@ Task 2
 Using Visual Studio, create a project using the Console Application template.
 Given two numbers A and B (A <B) print the sum of all the numbers located between these numbers on the screen.
 Given two numbers A and B (A <B) print all the odd values ​​located between the given numbers.
+
+
+Solution
+
+We ask the person to enter the first and second number. We read what numbers the user entered, convert to Int32. Using a loop, create a condition. In the body of the loop, create a variable and assign it the result of adding numbers between the specified user numbers. When the condition in the loop becomes false, display the sum of all numbers on the screen.
