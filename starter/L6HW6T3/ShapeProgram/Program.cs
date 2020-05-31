b6222901fb65a7f2bd88ec1ada5748b5dd4fef53
velmin <webseo89@gmail.com>
2020-05-31 23:16:37 +0300

    RectangleShapeProgram

--- starter/L6HW6T3/ShapeProgram/Program.cs
+++ starter/L6HW6T3/ShapeProgram/Program.cs
@@ -6,6 +6,19 @@ namespace ShapeProgram
     {
         static void Main()
         {
+            //create rectangle
+            for (int col = 0; col < 10; col++)
+            {
+
+                for (int r = 0; r < 30; r++)
+                {
+                    //display on screen
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
