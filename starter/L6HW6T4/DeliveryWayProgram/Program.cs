afac718bd66430098dd14cae206bec2c3cdb03aa
velmin <webseo89@gmail.com>
2020-06-04 21:22:13 +0300

    DeliveryWayProgram

--- starter/L6HW6T4/DeliveryWayProgram/Program.cs
+++ starter/L6HW6T4/DeliveryWayProgram/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace DeliveryWayProgram
+﻿using System;
+using System.Diagnostics;
+
+namespace DeliveryWayProgram
 {
     class Program
     {
         static void Main()
-        {
+        {   //display on screen string
+            Console.Write("Enter the number of customers for delivery: ");
+            //input user numeral
+            int counter = Convert.ToInt32((Console.ReadLine()));
+            int factorial = 1;
+            //negative check
+            if (counter < 0)
+            {   //Error - negative check
+                Console.WriteLine("The number of customers cannot be negative. Try again!");
+            }
+            else
+            {
+                do
+                {
+                    factorial *= counter--;
+                }
+
+                while (counter > 0);
+
+                {   //display on screen result
+                    Console.Write("Possible delivery routes: {0}", factorial);
+                }
+            }
+            //Delay
+            Console.ReadKey();
         }
     }
 }
