0735809bb94aa13cfce8dd03e5910759d155e10a
velmin <webseo89@gmail.com>
2020-06-04 21:37:28 +0300

    TriangleDotNet

--- starter/L6HW6T4/ShapeTriangleDotNet/Program.cs
+++ starter/L6HW6T4/ShapeTriangleDotNet/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace ShapeTriangleDotNet
+﻿using System;
+
+namespace ShapeTriangleDotNet
 {
     class Program
     {
         static void Main()
         {
-            
+            //start printing characters in a string
+            int g = 24;
+
+            //Characters
+            int z = 2;
+            //8 lines
+            for (int i = 0; i < 8; i++)
+            {
+                //print in line
+                for (int j = 0; j < 52; j++)
+                {
+                    Console.Write(" ");
+                    if (j == g)
+                    {
+                        for (int s = 0; s < z; s++)
+                            Console.Write("*");
+                    }
+                }
+                g -= 1;
+                z += 2;
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
         }
     }
 }
--- starter/L6HW6T4/ShapeTriangleDotNet/ReadMe.txt
+++ starter/L6HW6T4/ShapeTriangleDotNet/ReadMe.txt
@@ -2,3 +2,5 @@ Task 3
 Using Visual Studio 2010, create a project using the Console Application template.
 Using loops and method:
 Console.Write ("*"), Console.Write (""), Console.Write ("\ n") (to go to a new line).
+
++ Solution
