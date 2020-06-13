f87d7ff5acb95a363bfc2e5ec589b82d537d120e
velmin <webseo89@gmail.com>
2020-06-13 12:15:50 +0300

    Method Calculate Program

--- starter/L7HW7ADD/MethodCalculateProgram/Program.cs
+++ starter/L7HW7ADD/MethodCalculateProgram/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace MethodCalculateProgram
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MethodCalculateProgram
 {
     class Program
     {
-        static void Main()
+        //create method Calculate - signature and body
+        static int Calculate(int first, int second, int third)
         {
+            //return the value of the method arguments
+            return (first + second + third) / 2;
+        }
+
+        static void Main()
+        {  //set the value to our variables
+            int first = 2, second = 3, third = 5;
+            int result = Calculate(first, second, third);
+            Console.WriteLine("Arithmetic mean of argument values: {0}", result);
+           Console.Write("Press any key to close console window"); 
+            Console.ReadKey();
+
         }
     }
 }
--- starter/L7HW7ADD/ReadMe.txt
+++ starter/L7HW7ADD/ReadMe.txt
@@ -1,3 +1,9 @@
 Addition Taks
 Using Visual Studio, create a project using the ConsoleApplication template.
 Create a method called Calculate, which takes three integer arguments as parameters and displays the arithmetic mean of the argument values.
+
+Solutions:
++ Create method Calculate - signature and body. Using the return operator we return the value of the method.
++ Create method Main - signature and body. In this method set the value to our variables. Display on screen result Arithmetic mean of argument values.
+
+Done!
