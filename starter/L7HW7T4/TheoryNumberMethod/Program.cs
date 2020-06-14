0d570e36e10a6abc56a39747524c6331901f65bb
velmin <webseo89@gmail.com>
2020-06-14 18:11:17 +0300

    Positive / Negative Number Method

--- starter/L7HW7T4/TheoryNumberMethod/Program.cs
+++ starter/L7HW7T4/TheoryNumberMethod/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace TheoryNumberMethod
+﻿using System;
+
+namespace TheoryNumberMethod
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Number(int x)
         {
+            //positive or negative check
+            Console.WriteLine(x >= 0 ? "You entered a positive number" : "You entered a negative number");
+        }
+        static void Main()
+        {
+            Console.Write("Enter the number: ");
+
+            string value = Console.ReadLine();
+            int userNum = Convert.ToInt32(value);
+            Number(userNum);
+            //delay
+            Console.ReadKey();
         }
     }
 }
