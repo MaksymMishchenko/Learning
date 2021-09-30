779bf0685d33f8a06f3b51678fa2acc84add9b77
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-09-30 15:42:31 +0300

    Add tests for methods which convert to pound, frank, mark, yen

---
+++ Task/CurrencyConverterApp/CurrencyConverterApp/CurrencyConverterApp/CurrencyConvertor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverterApp
+{
+    public class CurrencyConvertor
+    {
+        private float _dollar = 10.0F;
+        private float _pound = 1.487F;
+        private float _franc = 0.172F;
+        private float _mark = 0.584F;
+        private float _yen = 0.00955F;
+
+        public float ConvertDollarToPound(float dollar, float pound)
+        {
+            return dollar / pound;
+        }
+
+        public float ConvertDollarToFranc(float dollar, float franc)
+        {
+            return dollar / franc;
+        }
+
+        public float ConvertDollarToMark(float dollar, float mark)
+        {
+            return dollar / mark;
+        }
+
+        public float ConvertDollarToYen(float dollar, float yen)
+        {
+            return dollar / yen;
+        }
+    }
+}
