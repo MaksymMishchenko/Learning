60b0fb94cff6befcca33c35ba67333357caabcfb
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-10-01 17:18:31 +0300

    Add test for task

---
+++ Task/TemperatureConvertorApp/TemperatureConvertorApp/TemperatureConvertorApp/TemperatureConvertor.cs
@@ -0,0 +1,10 @@
+﻿namespace TemperatureConvertorApp
+{
+    public class TemperatureConvertor : ITemperatureConvertor
+    {
+        public int CelsiusToFahrenheit(int celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+    }
+}
--- Task/TemperatureConvertorApp/TemperatureConvertorApp/TemperatureConvertorAppTests/TemperatureConvertorTests.cs
+++ Task/TemperatureConvertorApp/TemperatureConvertorApp/TemperatureConvertorAppTests/TemperatureConvertorTests.cs
@@ -1,13 +1,28 @@
 using Xunit;
+using TemperatureConvertorApp;
 
 namespace TemperatureConvertorAppTests
 {
     public class TemperatureConvertorTests
     {
+        private readonly TemperatureConvertor _target;
+
+        public TemperatureConvertorTests()
+        {
+            _target = new TemperatureConvertor();
+        }
         [Fact]
         public void Celsius_25C_ToFahrenheit_77F()
         {
+            //arrange
+            int celsius = 25;
+            int expected = 77;
 
+            //action
+            int actual = _target.CelsiusToFahrenheit(celsius);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
