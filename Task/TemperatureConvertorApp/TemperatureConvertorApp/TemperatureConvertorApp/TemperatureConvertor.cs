e4e2f4dc1fd840938b23cde4ad8308bcc22bd8a5
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-10-01 17:33:39 +0300

    Add TemperatureConvertorApp done

--- Task/TemperatureConvertorApp/TemperatureConvertorApp/TemperatureConvertorApp/TemperatureConvertor.cs
+++ Task/TemperatureConvertorApp/TemperatureConvertorApp/TemperatureConvertorApp/TemperatureConvertor.cs
@@ -2,6 +2,7 @@
 {
     public class TemperatureConvertor : ITemperatureConvertor
     {
+        //10 минут метод, 30 минут тест и решение задачи
         public int CelsiusToFahrenheit(int celsius)
         {
             return (celsius * 9 / 5) + 32;
