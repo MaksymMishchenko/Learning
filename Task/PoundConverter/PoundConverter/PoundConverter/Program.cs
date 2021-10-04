c86d9d640a8b22a079dc5a3836040de524b5fecf
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-10-04 11:47:44 +0300

    Add empty project and tests

---
+++ Task/PoundConverter/PoundConverter/PoundConverter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PoundConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Task/PoundConverter/PoundConverter/PoundConverterTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace PoundConverterTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
