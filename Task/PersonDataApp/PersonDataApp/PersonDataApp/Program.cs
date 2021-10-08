039a1d9d84aefc28f1e3c173769cbe27608e9c06
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-10-08 21:34:46 +0300

    implement program PersonDataApp

--- Task/PersonDataApp/PersonDataApp/PersonDataApp/Program.cs
+++ Task/PersonDataApp/PersonDataApp/PersonDataApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using PersonDataApp.Model;
 
 namespace PersonDataApp
 {
@@ -6,6 +8,21 @@ namespace PersonDataApp
     {
         static void Main(string[] args)
         {
+            List<Person> addPerson = new List<Person>();
+
+            addPerson.Add(new Person("Петров", "Василий", "Кленовая, 16", "Киев"));
+            addPerson.Add(new Person("Иванов", "Сергей", "Осиновая, 13", "Одесса"));
+            addPerson.Add(new Person("Сидоров", "Иван", "Осиновая, 13", "Житомир"));
+
+            ShowPerson show = new ShowPerson();
+
+            Console.WriteLine(" \tФамилия\tИмя\tАдресс\t \tГород");
+            Console.WriteLine(new string('-', 50));
+
+            foreach (var people in addPerson)
+            {
+                show.Show($" \t{people.Surname}\t{people.Name}\t{people.Address}\t{people.City}");
+            }
         }
     }
 }
