501ea1fcbe42545bf7d8da4086bb7dd29b035661
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-10-11 17:24:28 +0300

    added third column on the table

--- Task/MulTableApp/MulTableApp/MulTableApp/MulTableApp/Program.cs
+++ Task/MulTableApp/MulTableApp/MulTableApp/MulTableApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Channels;
 
 namespace MulTableApp
@@ -7,47 +8,56 @@ namespace MulTableApp
     {
         static void Main()
         {
-            string[][] firstRow = new string[2][];
-            {
-                firstRow[0] = new string[10]
-                {
-                    "2 *  1 =  2",
-                    "2 *  2 =  4",
-                    "2 *  3 =  6",
-                    "2 *  4 =  8",
-                    "2 *  5 = 10",
-                    "2 *  6 = 12",
-                    "2 *  7 = 14",
-                    "2 *  8 = 16",
-                    "2 *  9 = 18",
-                    "2 * 10 = 20"
-                };
+            string[] firstColumn = {
+                "2 *  1 =  2",
+                "2 *  2 =  4",
+                "2 *  3 =  6",
+                "2 *  4 =  8",
+                "2 *  5 = 10",
+                "2 *  6 = 12",
+                "2 *  7 = 14",
+                "2 *  8 = 16",
+                "2 *  9 = 18",
+                "2 * 10 = 20"
+            };
+            string[] secondColumn = {
 
-                firstRow[1] = new string[10]
-                {
-                    "3 *  1 =  3",
-                    "3 *  2 =  6",
-                    "3 *  3 =  9",
-                    "3 *  4 = 12",
-                    "3 *  5 = 15",
-                    "3 *  6 = 18",
-                    "3 *  7 = 21",
-                    "3 *  8 = 24",
-                    "3 *  9 = 27",
-                    "3 * 10 = 30"
-                };
+                "3 *  1 =  3",
+                "3 *  2 =  6",
+                "3 *  3 =  9",
+                "3 *  4 = 12",
+                "3 *  5 = 15",
+                "3 *  6 = 18",
+                "3 *  7 = 21",
+                "3 *  8 = 24",
+                "3 *  9 = 27",
+                "3 * 10 = 30"
             };
 
-            BuildTable(firstRow);
+            string[] thirdColumn = {
+
+                "4 *  1 =  4",
+                "4 *  2 =  8",
+                "4 *  3 = 12",
+                "4 *  4 = 16",
+                "4 *  5 = 20",
+                "4 *  6 = 24",
+                "4 *  7 = 28",
+                "4 *  8 = 32",
+                "4 *  9 = 36",
+                "4 * 10 = 40"
+            };
+
+            BuildTable(firstColumn,secondColumn,thirdColumn);
         }
 
-        public static void BuildTable(string[][] arr1)
+        public static void BuildTable(string[] arr1, string[] arr2, string[]arr3)
         {
             // левый верхний
             Console.Write($"{(char)9556}");
 
             // выводим верхнюю горизонталь таблицы
-            for (int j = 1; j < arr1.Length+26; j++)
+            for (int j = 1; j < arr1.Length+arr2.Length+arr3.Length+10; j++)
             {
                 if (j != 13)
                 {
@@ -65,25 +75,16 @@ namespace MulTableApp
             // данные помещенные в таблицу
             for (int i = 0; i < arr1.Length; i++)
             {
-                for (int j = 0; j < arr1[i].Length; j++)
-                {
-                    Console.Write($"{(char)9553}");
-                    Console.Write($"{arr1[i][j]}");
-                    Console.WriteLine();
-                }
-
-                // разделитель между столбиками
-                //
+                Console.Write($"{(char)9553}");
+                Console.Write($"{arr1[i]} {(char)9474} {arr2[i]} {(char)9474} {arr3[i]}");
+                Console.WriteLine($"{(char)9553}");
             }
-
-            //выводим правую вертикаль
-            Console.WriteLine($"{(char)9553}");
 
             // выводим левый нижний угол таблицы
             Console.Write($"{(char)9562}");
 
             // выводим нижнюю горизонталь таблицы
-            for (int j = 0; j < arr1.Length+25; j++)
+            for (int j = 0; j < arr1.Length+arr2.Length+arr3.Length+9; j++)
             {
                 if (j != 12)
                 {
