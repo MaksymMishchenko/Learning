22dee1ddcb24c9d8b568da2abfc86b283ae5b46c
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-10-15 18:38:20 +0300

    added program ColorTimeApp

--- Task/ColorTimeApp/ColorTimeApp/ColorTimeApp/Program.cs
+++ Task/ColorTimeApp/ColorTimeApp/ColorTimeApp/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace ColorTimeApp
+﻿using System;
+
+namespace ColorTimeApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(DateTime.Now);
+            
+            do
+            {
+                Console.WriteLine("Введите цвет времени: r-красный, y-желтый, g-зеленый");
+                string color = Console.ReadLine();
+                var colors = new Color();
+                colors.ChangeColor(color);
+
+            }
+
+            while (!(Console.ReadKey().Key == ConsoleKey.Q));
         }
     }
 }
