908cebbbe6d80eb8b32c578871748c0fe6aa51db
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-10-15 11:09:01 +0300

    implemented method create which return string jagged array completely

--- Task/MulTableApp/MulTableApp/MulTableApp/MulTableApp/MultiplicationTableGenerator.cs
+++ Task/MulTableApp/MulTableApp/MulTableApp/MulTableApp/MultiplicationTableGenerator.cs
@@ -18,9 +18,9 @@ namespace MulTableApp
             array[6] = new string[10];
             array[7] = new string[10];
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0, d = from ; i < till-1; i++, d++)
             {
-                for (int j = 0, a=2, b=1, n=0; j < array[i].Length; j++, b++)
+                for (int j = 0, a=d, b=1, n=0; j < array[i].Length; j++, b++)
                 {
                     var c = a * b;
                     array[i][j] = n.ToString($"{a}*{b}={c}");
