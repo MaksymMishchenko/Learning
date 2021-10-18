9c8d4b740166284b0a337bbbd64eff5d13e1d033
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-10-18 22:18:42 +0300

    added tests for arithmetic methods

---
+++ Task/ArithmeticCalculatorApp/ArithmeticCalculatorApp/ArithmeticCalculatorApp/Calculator.cs
@@ -0,0 +1,30 @@
+﻿namespace ArithmeticCalculatorApp
+{
+    public class Calculator
+    {
+        public double Adds(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multipliplies(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divides(double a, double b)
+        {
+            return a / b;
+        }
+
+        public double DividesRemainder(double a, double b)
+        {
+            return a % b;
+        }
+    }
+}
