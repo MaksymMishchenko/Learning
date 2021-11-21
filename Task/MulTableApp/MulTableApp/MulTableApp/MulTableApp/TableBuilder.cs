6a876f456c13b064613105deb3396d5dbd8e1cbf
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-11-21 17:32:42 +0200

    implement method which displays the multiplication table

--- Task/MulTableApp/MulTableApp/MulTableApp/MulTableApp/TableBuilder.cs
+++ Task/MulTableApp/MulTableApp/MulTableApp/MulTableApp/TableBuilder.cs
@@ -19,16 +19,20 @@ namespace MulTableApp
             // ╔
             Console.Write($"{(char)9556}");
 
-            for (int i = 0; i < param.Width * content.Length; i++)
+            for (int i = 0; i < 47; i++)
             {
-                if (i % param.Width == 0 && i != 0)
+                if (i == 11 || i == 23 || i == 35)
                 {
                     // ╤
                     Console.Write($"{(char)9572}");
                 }
+                else
+                {
+                    // ═
+                    Console.Write($"{(char)9552}");
+                }
 
-                // ═
-                Console.Write($"{(char)9552}");
+
             }
             // ╗
             Console.Write($"{(char)9559}");
@@ -38,41 +42,88 @@ namespace MulTableApp
             {
                 // ║
                 Console.Write($"{(char)9553}");
-                for (int i = 0; i < content.Length; i++)
+                for (int i = 0; i < content.Length / 2; i++)
                 {
                     for (int j = n; j < content[i].Length - m; j++)
                     {
                         Console.Write($"{content[i][j]}");
 
-                        
-                            if (i == 3)
-                            {
-                                // ║
-                                Console.Write($"{(char)9553}");
-                            }
-                            else
-                            {
-                                // │
-                                Console.Write($"{(char)9474}");
-                            }
-                            
-                        
+                        if (i == 3)
+                        {
+                            // ║
+                            Console.Write($"{(char)9553}");
+                        }
+                        else
+                        {
+                            // │
+                            Console.Write($"{(char)9474}");
+                        }
                     }
                 }
                 Console.WriteLine();
             }
+
+            // ╠
+            Console.Write($"{(char)9568}");
+
+            for (int i = 0; i < 47; i++)
+            {
+                if (i == 11 || i == 23 || i == 35)
+                {
+                    // ╡
+                    Console.Write($"{(char)9578}");
+                }
+                else
+                {
+                    // ═
+                    Console.Write($"{(char)9552}");
+                }
+            }
+
+            // ╣
+            Console.Write($"{(char)9571}");
+            Console.WriteLine();
+
+            for (int n = 0, m = 8; n < content.Length + 1; n++, m--)
+            {
+                // ║
+                Console.Write($"{(char)9553}");
+                for (int i = 4; i < content.Length; i++)
+                {
+                    for (int j = n; j < content[i].Length - m; j++)
+                    {
+                        Console.Write($"{content[i][j]}");
+
+                        if (i == 7)
+                        {
+                            // ║
+                            Console.Write($"{(char)9553}");
+                        }
+                        else
+                        {
+                            // │
+                            Console.Write($"{(char)9474}");
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+
             // ╚
             Console.Write($"{(char)9562}");
 
-            for (int i = 0; i < param.Width * content.Length; i++)
+            for (int i = 0; i < 47; i++)
             {
-                if (i % param.Width == 0 && i != 0)
+                if (i == 11 || i == 23 || i == 35)
                 {
                     // ╧
                     Console.Write($"{(char)9575}");
                 }
-                // ═
-                Console.Write($"{(char)9552}");
+                else
+                {
+                    // ═
+                    Console.Write($"{(char)9552}");
+                }
             }
 
             // ╝
