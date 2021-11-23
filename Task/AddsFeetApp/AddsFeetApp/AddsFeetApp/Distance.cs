b819b4e22a0c3320da38058a33b4d1a90352d912
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-11-23 15:35:14 +0200

    Added methodSum and Test Class. Test passed

---
+++ Task/AddsFeetApp/AddsFeetApp/AddsFeetApp/Distance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AddsFeetApp
+{
+    public struct Distance
+    {
+        private int _feet;
+        private float _inches;
+        private const int _oneFeet = 12;
+
+        public Distance(int feet, float inches)
+        {
+            _feet = feet;
+            _inches = inches;
+        }
+
+        public void SumFeetsAndInches(int feet, float inches, ref int tempFeet, ref float tempInches)
+        {
+            tempFeet = 0;
+            float tepmInches = _inches + inches;
+
+            if (tepmInches >= _oneFeet)
+            {
+                tepmInches -= _oneFeet;
+                tempFeet++;
+            }
+
+            tempInches = tepmInches;
+            tempFeet += _feet + feet;
+        }
+    }
+}
--- Task/AddsFeetApp/AddsFeetApp/AddsFeetApp/Program.cs
+++ Task/AddsFeetApp/AddsFeetApp/AddsFeetApp/Program.cs
@@ -6,7 +6,7 @@ namespace AddsFeetApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
