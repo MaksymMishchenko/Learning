99d0bb9d4e88665af58a7837b9f9e98bd465fe20
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-11-25 20:30:11 +0200

    Add finished program

--- SimpleCode/TriangleApp/TriangleApp/Program.cs
+++ SimpleCode/TriangleApp/TriangleApp/Program.cs
@@ -6,7 +6,17 @@ namespace TriangleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите высоту треугольника: ");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
