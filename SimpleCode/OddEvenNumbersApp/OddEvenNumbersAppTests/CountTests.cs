301d2ba4a5ca996e4904e930eda829550e280a3d
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-11-25 13:25:41 +0200

    Add successful tests

--- SimpleCode/OddEvenNumbersApp/OddEvenNumbersAppTests/CountTests.cs
+++ SimpleCode/OddEvenNumbersApp/OddEvenNumbersAppTests/CountTests.cs
@@ -1,14 +1,44 @@
 using System;
+using OddEvenNumbersApp;
 using Xunit;
 
 namespace OddEvenNumbersAppTests
 {
     public class CountTests
     {
-        [Fact]
-        public void Test1()
+        private readonly Count _target;
+        public CountTests()
         {
+            _target = new Count();
+        }
 
+        [Fact]
+        public void Count1_10expected5_5Sum25_30()
+        {
+            // arrange
+            int currentValue = 1;
+            int limit = 10;
+
+            int oddNumbersCount = 0;
+            int evenNumbersCount = 0;
+
+            int oddNumberSum = 0;
+            int evenNumberSum = 0;
+
+            int oddNumbersExpected = 5;
+            int evenNumbersExpected = 5;
+
+            int oddSumExpected = 25;
+            int evenSumExpected = 30;
+
+            // actual
+            _target.GetOddEvenNumbers(currentValue, limit, ref oddNumbersCount, ref evenNumbersCount, ref oddNumberSum, ref evenNumberSum);
+
+            // assert
+            Assert.Equal(oddNumbersExpected, oddNumbersCount);
+            Assert.Equal(evenNumbersExpected, evenNumbersCount);
+            Assert.Equal(oddSumExpected, oddNumberSum);
+            Assert.Equal(evenSumExpected, evenNumberSum);
         }
     }
 }
