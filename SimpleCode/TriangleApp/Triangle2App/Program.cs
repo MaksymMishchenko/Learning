ca893733dfbc7b90cfbfaa568e8ceb8f66203caf
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-11-25 20:37:57 +0200

    Add finished Triangle2App program

--- SimpleCode/TriangleApp/Triangle2App/Program.cs
+++ SimpleCode/TriangleApp/Triangle2App/Program.cs
@@ -6,7 +6,17 @@ namespace Triangle2App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите высоту перевенутого треугольника: ");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
