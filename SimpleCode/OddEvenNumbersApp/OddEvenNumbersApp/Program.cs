19f5310d6dbfc83608d45db176fb2cf70cac6bbe
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-11-25 13:34:56 +0200

    Add finished program

--- SimpleCode/OddEvenNumbersApp/OddEvenNumbersApp/Program.cs
+++ SimpleCode/OddEvenNumbersApp/OddEvenNumbersApp/Program.cs
@@ -6,7 +6,41 @@ namespace OddEvenNumbersApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Clear();
+                try
+                {
+                    Console.WriteLine("Программа подсчета количества и суммы четных и нечетных чисел :)");
+                    Console.WriteLine("Введите начальное число:");
+                    int currentValue = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите последнее число:");
+                    int limit = int.Parse(Console.ReadLine());
+
+                    int oddNumbersCount = 0;
+                    int evenNumbersCount = 0;
+
+                    int oddNumberSum = 0;
+                    int evenNumberSum = 0;
+
+                    Count count = new Count();
+                    count.GetOddEvenNumbers(currentValue, limit, ref oddNumbersCount, ref evenNumbersCount,
+                        ref oddNumberSum, ref evenNumberSum);
+
+                    Console.WriteLine("Количество нечетных чисел: " + oddNumbersCount);
+                    Console.WriteLine("Количество четных чисел: " + evenNumbersCount);
+
+                    Console.WriteLine("Сумма нечетных чисел: " + oddNumberSum);
+                    Console.WriteLine("Сумма четных чисел: " + evenNumberSum);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Невозможно преобразовать строку в число! ");
+
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
