6d016f8890ab9b1f4bd628f69acdb1133f2bc9cc
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-11-25 20:33:12 +0200

    Add empty Triangle2App project

---
+++ SimpleCode/TriangleApp/Triangle2App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Triangle2App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
