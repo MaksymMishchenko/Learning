adbe129caeb8b993b4f1b9ffeae3623ff2b88705
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-11-25 20:25:45 +0200

    Add empty project

---
+++ SimpleCode/TriangleApp/TriangleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TriangleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
