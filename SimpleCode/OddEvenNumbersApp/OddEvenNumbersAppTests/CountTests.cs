15d35f33cbc2b332a2524297ccef0f1e7e82b27d
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-11-25 13:04:51 +0200

    Add empty program and test project

---
+++ SimpleCode/OddEvenNumbersApp/OddEvenNumbersAppTests/CountTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OddEvenNumbersAppTests
+{
+    public class CountTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
