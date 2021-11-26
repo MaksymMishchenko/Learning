a05b1a8eebf25793634ade1656037f8163c41603
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-11-26 13:38:39 +0200

    Add empty program and test project

---
+++ SimpleCode/ArrayApp/ArrayApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SimpleCode/ArrayApp/ArrayAppTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ArrayAppTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
