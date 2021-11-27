0d668126ad4dd2d8ced87c1680fdbfa6b028c1d2
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-11-27 19:06:14 +0200

    Add empty program and tests project

---
+++ SimpleCode/ArrayApp/SecSmallValueApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecSmallValueApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SimpleCode/ArrayApp/SecSmallValueAppTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SecSmallValueAppTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
