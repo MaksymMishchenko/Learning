d87be90484c172a76c20c7fc02d8fdfc089ab727
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-11-29 20:12:48 +0200

    Add empty program and test project

---
+++ SimpleCode/MethodsApp/MethodsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MethodsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
