25a64cec9537335c614de5ac6d4ae793e300a752
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-03 20:33:35 +0200

    Add finished program RemoveAtArrayApp

--- SimpleCode/RefArrayApp/RemoveAtArrayApp/Program.cs
+++ SimpleCode/RefArrayApp/RemoveAtArrayApp/Program.cs
@@ -4,9 +4,39 @@ namespace RemoveAtArrayApp
 {
     class Program
     {
+        static void RemoveAt(ref int[] arr, int index)
+        {
+            int[] newArray = new int[arr.Length - 1];
+
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = arr[i];
+            }
+
+            for (int i = index+1; i < arr.Length; i++)
+            {
+                newArray[i-1] = arr[i];
+            }
+
+            arr = newArray;
+        }
+
+        static void RemoveFirst(ref int[] arr)
+        {
+            RemoveAt(ref arr, 0);
+        }
+
+        static void RemoveLast(ref int[] arr)
+        {
+            RemoveAt(ref arr, arr.Length-1);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+            RemoveAt(ref arr, 3);
+            RemoveFirst(ref arr);
+            RemoveLast(ref arr);
         }
     }
 }
