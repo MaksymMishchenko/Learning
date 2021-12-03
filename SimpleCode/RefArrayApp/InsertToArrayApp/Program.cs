26d87b6c045652ce1b52b22b28dce59b15bb97e6
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-03 20:13:48 +0200

    Add finished program InsertToArrayApp

--- SimpleCode/RefArrayApp/InsertToArrayApp/Program.cs
+++ SimpleCode/RefArrayApp/InsertToArrayApp/Program.cs
@@ -4,9 +4,40 @@ namespace InsertToArrayApp
 {
     class Program
     {
+        static void Insert(ref int[] arr, int value, int index)
+        {
+            int[] newArray = new int[arr.Length + 1];
+            newArray[index] = value;
+
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = arr[i];
+            }
+
+            for (int i = index; i < arr.Length; i++)
+            {
+                newArray[i + 1] = arr[i];
+            }
+
+            arr = newArray;
+        }
+
+        static void InsertFirst(ref int[] arr, int value)
+        {
+            Insert(ref arr, value, 0);
+        }
+
+        static void InsertLast(ref int[] arr, int value)
+        {
+            Insert(ref arr, value, arr.Length);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 2, 3, 5, 8, 9 };
+            Insert(ref arr, 10, 3);
+            InsertFirst(ref arr, 1);
+            InsertLast(ref arr, 11);
         }
     }
 }
