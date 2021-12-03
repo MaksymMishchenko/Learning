ed0c2eb5e261e28e5fc46211e7a983ee97909c74
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-03 19:41:16 +0200

    Add finished program

--- SimpleCode/RefArrayApp/RefArrayApp/Program.cs
+++ SimpleCode/RefArrayApp/RefArrayApp/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace RefArrayApp
 {
     class Program
     {
+        static void Resize<T>(ref T[] arr, int newSize)
+        {
+            T[] newArray = new T[newSize];
+
+            for (int i = 0; i < arr.Length && i < newArray.Length; i++)
+            {
+                newArray[i] = arr[i];
+            }
+            arr = newArray;
+        }
+
+        static void PrintArray<T>(T[] arr)
+        {
+            foreach (var el in arr)
+            {
+                Console.Write($"{el}\t");
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // массив int
+            int[] arr = { 1, 2, 3 };
+            Console.WriteLine($"Размер массива (int) до увеличения: {arr.Length}");
+            PrintArray(arr);
+
+            Resize(ref arr, 7);
+            Console.WriteLine($"Размер массива (int) после увеличения: {arr.Length}");
+            PrintArray(arr);
+
+            Resize(ref arr, 2);
+            Console.WriteLine($"Размер массива (int) после уменьшения: {arr.Length}");
+            PrintArray(arr);
+
+            Console.WriteLine(new string('-', 50));
+
+            // массив string
+            string[] stringArr = { "One", "Two", "Three" };
+
+            Console.WriteLine($"Размер массива (string) до увеличения: {stringArr.Length}");
+            PrintArray(stringArr);
+
+            Resize(ref stringArr, 7);
+            Console.WriteLine($"Размер массива (string) после увеличения: {stringArr.Length}");
+            PrintArray(stringArr);
+
+            Resize(ref stringArr, 2);
+            Console.WriteLine($"Размер массива (string) после уменьшения: {stringArr.Length}");
+            PrintArray(stringArr);
         }
     }
 }
