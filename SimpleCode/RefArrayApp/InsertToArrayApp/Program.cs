cba0dabaef5db6d68db92dfb95f5f98ec0bafccc
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-03 19:54:14 +0200

    Add empty project InsertToArrayApp and readme file

---
+++ SimpleCode/RefArrayApp/InsertToArrayApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InsertToArrayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
