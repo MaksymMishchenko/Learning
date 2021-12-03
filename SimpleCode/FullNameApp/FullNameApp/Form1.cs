cc7f8622a8e33e7793ca14da34d87ab08cae39ec
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-03 11:36:52 +0200

    Add finisged project

--- SimpleCode/FullNameApp/FullNameApp/Form1.cs
+++ SimpleCode/FullNameApp/FullNameApp/Form1.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FullNameApp
@@ -15,5 +11,26 @@ namespace FullNameApp
             InitializeComponent();
         }
 
+        private List<Person> GetPersons()
+        {
+            return new List<Person>()
+
+            {
+                new Person(){FirstName = "Максим", MiddleName = "Владимирович", SecondName = "Сергейчук"},
+                new Person(){FirstName = "Steve", SecondName = "Martin"},
+                new Person(){FirstName = "Jeffrey", SecondName = "Richter"},
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            var persons = GetPersons();
+
+            foreach (var person in persons)
+            {
+                listBox1.Items.Add(person.GetFulName());
+            }
+        }
     }
 }
---
+++ SimpleCode/FullNameApp/FullNameApp/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace FullNameApp
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string SecondName { get; set; }
+
+        public string GetFulName()
+        {
+            return $"Фамилия: {SecondName ?? "Нет данных!"} | Имя:  {FirstName ?? "Нет данных!"} | Отчество: {MiddleName ?? "Нет данных!"}";
+        }
+    }
+}
