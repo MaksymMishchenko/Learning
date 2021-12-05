2e79c99d01d55d1b645ce4b9a8ea7c8e711cdfb6
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-05 13:12:04 +0200

    Add finished program RecursionPrintArrayApp

--- SimpleCode/RecursionSumArrayApp/RecursionPrintArrayApp/Program.cs
+++ SimpleCode/RecursionSumArrayApp/RecursionPrintArrayApp/Program.cs
@@ -4,9 +4,19 @@ namespace RecursionPrintArrayApp
 {
     class Program
     {
+        private static void PrintArray(int[] arr, int i = 0)
+        {
+            if (i < arr.Length)
+            {
+                Console.WriteLine(arr[i]);
+                PrintArray(arr, i + 1);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            PrintArray(arr);
         }
     }
 }
