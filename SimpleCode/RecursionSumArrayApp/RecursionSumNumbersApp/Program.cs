db426648cfdf0497f96e76e213fe6d785141d0b5
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-05 13:23:44 +0200

    RecursionSumNumbersApp

--- SimpleCode/RecursionSumArrayApp/RecursionSumNumbersApp/Program.cs
+++ SimpleCode/RecursionSumArrayApp/RecursionSumNumbersApp/Program.cs
@@ -4,9 +4,23 @@ namespace RecursionSumNumbersApp
 {
     class Program
     {
+        private static int SumNubers(int value)
+        {
+            if (value < 10)
+            {
+                return value;
+            }
+
+            int digit = value % 10;
+            int nextValue = value / 10;
+            return digit + SumNubers(nextValue);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int value = 561;
+            var sumNumbers = SumNubers(value);
+            Console.WriteLine(sumNumbers);
         }
     }
 }
