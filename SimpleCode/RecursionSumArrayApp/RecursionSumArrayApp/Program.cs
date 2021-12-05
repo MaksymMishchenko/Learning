152cab90b54a0723615773302aa3f7d5e801d5fe
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-05 13:00:09 +0200

    Add finished project RecursionSumArrayApp

--- SimpleCode/RecursionSumArrayApp/RecursionSumArrayApp/Program.cs
+++ SimpleCode/RecursionSumArrayApp/RecursionSumArrayApp/Program.cs
@@ -4,9 +4,22 @@ namespace RecursionSumArrayApp
 {
     class Program
     {
+        static int Sum(int[] arr, int i = 0)
+        {
+            if (i >= arr.Length)
+            {
+                return 0;
+            }
+
+            return arr[i] + Sum(arr, i + 1);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 1, 2, 3, 6, 8 };
+
+            var result = Sum(arr);
+            Console.WriteLine(result);
         }
     }
 }
