d533461d1d0e3d85cd1441c8e088f1ee5b92b254
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-07 16:57:24 +0200

    Add empty project and readme file

---
+++ SimpleCode/GunApp/GunApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GunApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
