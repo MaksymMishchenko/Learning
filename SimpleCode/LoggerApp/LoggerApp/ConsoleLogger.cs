9949d0236697c7ec47776edaec4744361c936ce5
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-11 16:43:39 +0200

    Add finished program

---
+++ SimpleCode/LoggerApp/LoggerApp/ConsoleLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LoggerApp
+{
+    class ConsoleLogger : IConsoleLogger
+    {
+        public void Log(LogLevel logLevel, string message)
+        {
+            switch (logLevel)
+            {
+                case LogLevel.Debug:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case LogLevel.Info:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogLevel.Warning:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                case LogLevel.Error:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case LogLevel.Fatal:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                default:
+                    Console.WriteLine("Unknown LogLevel");
+                    break;
+            }
+
+            Console.WriteLine($"{DateTime.Now}, message: {message}");
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+    }
+}
--- SimpleCode/LoggerApp/LoggerApp/Program.cs
+++ SimpleCode/LoggerApp/LoggerApp/Program.cs
@@ -6,7 +6,16 @@ namespace LoggerApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IConsoleLogger logger = new ConsoleLogger();
+            logger.Log(LogLevel.Debug, "some Debug level");
+            logger.Log(LogLevel.Info, "some Info level");
+            logger.Log(LogLevel.Warning, "some Warning level");
+            logger.Log(LogLevel.Error, "some Error level");
+            logger.Log(LogLevel.Fatal, "some Fatal level");
+
+            // Используем метод, который реализовали прямо в интерфейсе
+            logger.ShowEmployee(Employees.Boris);
+            logger.ShowEmployee(Employees.Nikolay);
         }
     }
 }
