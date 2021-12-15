74a85b3be61ba9da49f36ac2c89dec6b4233ba93
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-15 16:19:13 +0200

    Add finished project

--- SimpleCode/StringApp/StringApp/Program.cs
+++ SimpleCode/StringApp/StringApp/Program.cs
@@ -6,7 +6,27 @@ namespace StringApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = new int[10];
+            string[] arrString = { "Нуль", "Один", "Два", "Три", "Четыре", "Пять", "Шесть", "Семь", "Восемь", "Девять" };
+            int nextDigit;
+            int index = 0;
+
+            int userNumber = 5485;
+
+            do
+            {
+                nextDigit = userNumber % 10;
+                numbers[index] = nextDigit;
+                index++;
+                userNumber = userNumber / 10;
+            } while (userNumber > 0);
+
+            index--;
+
+            for (; index >= 0; index--)
+            {
+                Console.Write(arrString[numbers[index]] + " ");
+            }
         }
     }
 }
