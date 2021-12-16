94e66bc49448ea92a7eb3a0bb0891293114f56dd
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-16 22:08:44 +0200

    Add tests for GetSmallestNumeric method

---
+++ SimpleCode/ArrayApp/SmallestNumApp/MyArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallestNumApp
+{
+    public class MyArray
+    {
+        public int[] CreateArray(int size)
+        {
+            return new int [size];
+        }
+
+        public void FillArray(int[] arr)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(100);
+            }
+        }
+
+        public int GetSmallestNumeric(int[] arr)
+        {
+            int min = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            return min;
+        }
+    }
+}
