bec08482448c9cf1143eb2a9e6d7374f4c203a3a
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-16 23:44:49 +0200

    Add finished program

--- SimpleCode/FactoryMethodApp/FactoryMethodApp/Program.cs
+++ SimpleCode/FactoryMethodApp/FactoryMethodApp/Program.cs
@@ -6,7 +6,17 @@ namespace FactoryMethodApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var r1 = new Rectangle(4, 5);
+            r1.Show();
+            int r1Area = r1.GetArea();
+            Console.WriteLine($"Rectangle Area: {r1Area}");
+
+            Console.WriteLine(new string('-', 50));
+
+            var r2 = r1.Factor(2);
+            r2.Show();
+            int r2Area =  r2.GetArea();
+            Console.WriteLine($"Rectangle Area with Factor: {r2Area}");
         }
     }
 }
