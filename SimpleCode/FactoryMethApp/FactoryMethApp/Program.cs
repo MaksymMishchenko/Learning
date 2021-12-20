0b801ae4c1aea079049f9d6970af85eb9e3b2cec
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-20 22:28:04 +0200

    Add empty project and readme file

---
+++ SimpleCode/FactoryMethApp/FactoryMethApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoryMethApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
