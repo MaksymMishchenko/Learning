54b97517e1d277324fdd437cc1632d8f9d931bb2
Maksym Mishchenko <mishchenkomv@hotmail.com>
2021-12-22 16:56:08 +0200

    Add example operator overloading true/false

---
+++ SimpleCode/OperatorOverloadApp/OperatorTrueFalseApp/Point.cs
@@ -0,0 +1,33 @@
+﻿namespace OperatorTrueFalseApp
+{
+    class Point
+    {
+        private int _x, _y;
+
+        public Point(int xPos, int yPos)
+        {
+            _x = xPos;
+            _y = yPos;
+        }
+
+        public static bool operator true(Point a)
+        {
+            if (a._x != 0 || a._y != 0)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator false(Point a)
+        {
+            if (a._x == 0 && a._y == 0)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ SimpleCode/OperatorOverloadApp/OperatorTrueFalseApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OperatorTrueFalseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point a = new Point(1, 1);
+            Point b = new Point(0, 2);
+            
+            if(a) Console.WriteLine("Истина");
+            if(b) Console.WriteLine("Истина");
+
+            Point c = new Point(0, 0);
+            if (c) Console.WriteLine("Истина");
+            else Console.WriteLine("Fake");
+        }
+    }
+}
