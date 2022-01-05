6dda908728d6184e5d0ac08cce5cb55f643df38b
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-05 19:51:35 +0200

    Add class Employee

---
+++ Task/EmployeesApp/EmployeesApp/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeesApp
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+        public DateTime StartWork { get; set; }
+    }
+}
