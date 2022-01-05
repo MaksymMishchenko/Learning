15a3ee59ed05c04ced3019167eab7917f6364754
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-05 19:57:13 +0200

    Add employees to collection

--- Task/EmployeesApp/EmployeesApp/Program.cs
+++ Task/EmployeesApp/EmployeesApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EmployeesApp
 {
@@ -6,7 +7,14 @@ namespace EmployeesApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var employees = new List<Employee>()
+            {
+                new Employee{FirstName = "Maks", LastName = "Mischenko", Salary = 100000, StartWork = DateTime.Parse("01/01/2005")},
+
+                new Employee{FirstName = "Petr", LastName = "Demchenko", Salary = 50000, StartWork = DateTime.Parse("01/01/2008")},
+
+                new Employee{FirstName = "Artur", LastName = "Petrov", Salary = 30000, StartWork = DateTime.Parse("01/01/2020")}
+            };
         }
     }
 }
