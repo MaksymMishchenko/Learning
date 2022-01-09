94866d96e03d94f766f67c0190ab6d94affa8e51
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-09 18:13:52 +0200

    Added Delegate 1 and 3 methods

--- Essential/ArrayDelegatesApp/ArrayDelegatesApp/Program.cs
+++ Essential/ArrayDelegatesApp/ArrayDelegatesApp/Program.cs
@@ -2,11 +2,38 @@
 
 namespace ArrayDelegatesApp
 {
+    public delegate int Delegate1(Delegate2[] arr);
+
+    public delegate int Delegate2();
     class Program
     {
+        private static Random rnd = new Random();
+        public static int Method1()
+        {
+            return rnd.Next(1, 15);
+        }
+
+        public static int Method2()
+        {
+            return rnd.Next(5,30);
+        }
+        public static int Method3()
+        {
+            return rnd.Next(10, 20);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Delegate2 method1 = Method1;
+            Delegate2 method2 = Method2;
+            Delegate2 method3 = Method3;
+
+            int meth1 = method1.Invoke();
+            int meth2 = method2.Invoke();
+            int meth3 = method3.Invoke();
+
+            var result = (meth1 + meth2 + meth3) / 3;
+            Console.WriteLine(result);
         }
     }
 }
