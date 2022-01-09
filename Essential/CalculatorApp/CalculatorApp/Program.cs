fee86e2ced5306ae867f0107e51e1f56130de085
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-09 16:50:50 +0200

    Added check division by Zero, input digit and operation from console

--- Essential/CalculatorApp/CalculatorApp/Program.cs
+++ Essential/CalculatorApp/CalculatorApp/Program.cs
@@ -9,12 +9,47 @@ namespace CalculatorApp
             ArithDelegate sum = (int a, int b) => a + b;
             ArithDelegate sub = (int a, int b) => a - b;
             ArithDelegate mul = (int a, int b) => a * b;
-            ArithDelegate div = (int a, int b) => a / b;
+            ArithDelegate div = (int a, int b) => IsZero(a) ? 0: a / b;
 
-            Console.WriteLine(sum.Invoke(2,5));
-            Console.WriteLine(sub.Invoke(10, 2));
-            Console.WriteLine(mul.Invoke(3,3));
-            Console.WriteLine(div.Invoke(10,2));
+            Console.WriteLine("Input first digit: ");
+            int digitA = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input operations: '+', '-', '*', '/'");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("Input second digit: ");
+            int digitB = int.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine("Operation result: ");
+                    Console.WriteLine(sum.Invoke(digitA, digitB));
+                    break;
+                case "-":
+                    Console.WriteLine("Operation result: ");
+                    Console.WriteLine(sub.Invoke(digitA, digitB));
+                    break;
+                case "*":
+                    Console.WriteLine("Operation result: ");
+                    Console.WriteLine(mul.Invoke(digitA, digitB));
+                    break;
+                case "/":
+                    Console.WriteLine("Operation result: ");
+
+                    Console.WriteLine(div.Invoke(digitA, digitB));
+                    break;
+                default:
+                    Console.WriteLine("Incorrect operation. Please, try again");
+                    break;
+            }
+        }
+
+        private static bool IsZero(int digitA)
+        {
+            if (digitA == 0)
+                Console.WriteLine("На нуль делить нельзя");
+            return false;
         }
     }
 }
