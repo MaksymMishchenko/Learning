d99afe743e1a09883486d929e350fec395560b6c
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-09 16:12:00 +0200

    Added implement lambda operators -sum, sub, mul, div

--- Essential/CalculatorApp/CalculatorApp/Program.cs
+++ Essential/CalculatorApp/CalculatorApp/Program.cs
@@ -6,7 +6,15 @@ namespace CalculatorApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ArithDelegate sum = (int a, int b) => a + b;
+            ArithDelegate sub = (int a, int b) => a - b;
+            ArithDelegate mul = (int a, int b) => a * b;
+            ArithDelegate div = (int a, int b) => a / b;
+
+            Console.WriteLine(sum.Invoke(2,5));
+            Console.WriteLine(sub.Invoke(10, 2));
+            Console.WriteLine(mul.Invoke(3,3));
+            Console.WriteLine(div.Invoke(10,2));
         }
     }
 }
