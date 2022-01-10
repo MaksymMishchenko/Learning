30259492bd61eb936ab29997772d9eae42a5c273
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-10 14:13:08 +0200

    Added finished MyList program

--- MyListApp/MyListApp/MyListApp/MyList.cs
+++ MyListApp/MyListApp/MyListApp/MyList.cs
@@ -4,28 +4,56 @@ namespace MyListApp
 {
     class MyList<T>
     {
-        public T[] newList = new T[0];
+        /// <summary>
+        /// Generic field
+        /// </summary>
+        private T[] _newList = new T[0];
 
+        /// <summary>
+        /// Indexer of the array
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public T this[int index] => _newList[index];
+
+        /// <summary>
+        /// Returns lenght of the list
+        /// </summary>
+        public int NewList
+        {
+            get { return _newList.Length; }
+        }
+
+        /// <summary>
+        /// Adds items to list
+        /// </summary>
+        /// <param name="number"></param>
         public void Add(T number)
         {
-            T[] newArray = new T[newList.Length + 1];
+            T[] newArray = new T[_newList.Length + 1];
 
             for (int i = 0; i < newArray.Length; i++)
             {
-                if (i < newList.Length)
+                if (i < _newList.Length)
                 {
-                    newArray[i] = newList[i];
+                    newArray[i] = _newList[i];
                 }
                 else
                 {
                     newArray[i] = number;
                 }
             }
-            newList = newArray;
+            _newList = newArray;
         }
+
+        /// <summary>
+        /// Inserts elements to List by index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="number"></param>
         public void Insert(int index, T number)
         {
-            T[] newArray = new T[newList.Length + 1];
+            T[] newArray = new T[_newList.Length + 1];
 
             for (int i = 0, j = 0; i < newArray.Length; i++)
             {
@@ -35,23 +63,26 @@ namespace MyListApp
                 }
                 else if (i != index)
                 {
-                    newArray[i] = newList[j];
+                    newArray[i] = _newList[j];
                     j++;
                 }
                 else
                 {
-                    newArray[i] = newList[i];
+                    newArray[i] = _newList[i];
                 }
             }
 
-            newList = newArray;
+            _newList = newArray;
         }
 
+        /// <summary>
+        /// Shows  items from the collection
+        /// </summary>
         public void Show()
         {
-            foreach (var el in newList)
+            foreach (var el in _newList)
             {
-                Console.WriteLine($"{el}");
+                Console.WriteLine($"Item: {el}");
             }
         }
     }
--- MyListApp/MyListApp/MyListApp/Program.cs
+++ MyListApp/MyListApp/MyListApp/Program.cs
@@ -1,22 +1,43 @@
-﻿namespace MyListApp
+﻿using System;
+
+namespace MyListApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // int List
             var myList = new MyList<int>();
             myList.Add(1);
             myList.Add(3);
             myList.Add(4);
 
-            var myListStr = new MyList<string>();
-            myListStr.Add("Hello");
-            myListStr.Add("World");
-            myListStr.Show();
-            
+            //Inserts elements to List by index
             myList.Insert(1, 2);
             myList.Insert(4, 5);
             myList.Show();
+            myList.Show();
+
+            Console.WriteLine(new string('-', 20));
+
+            // string list
+            var myListStr = new MyList<string>();
+            myListStr.Add("Hello");
+            myListStr.Add("Amazing");
+            myListStr.Add("World");
+            myListStr.Show();
+
+            Console.WriteLine(new string('-', 20));
+
+            //get list lenght
+            var listLenght = myListStr.NewList;
+            Console.WriteLine($"List Lenght: {listLenght}");
+
+            Console.WriteLine(new string('-', 20));
+
+            // get the item by index 
+            var ind = myListStr[1];
+            Console.WriteLine($"Item by index: {ind}");
         }
     }
 }
