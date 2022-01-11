06101a7234ec6d7b73f3e13f5a34531bd9329958
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-11 19:51:53 +0200

    Added new task to readme file

---
+++ Essential/ListExtensionMethodApp/ListExtensionMethodApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListExtensionMethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
