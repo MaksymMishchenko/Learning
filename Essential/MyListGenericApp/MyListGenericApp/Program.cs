ff8d0a4fa72d765df984dab3f892f16a6e72315e
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-12 20:17:48 +0200

    Added finished program

--- Essential/MyListGenericApp/MyListGenericApp/Program.cs
+++ Essential/MyListGenericApp/MyListGenericApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyListGenericApp
 {
@@ -6,7 +7,27 @@ namespace MyListGenericApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var myList = new MyList<string>();
+            myList.Add("Maks");
+            myList.Add("Dima");
+            myList.Add("Vova");
+            myList.Insert(3, "Petr");
+
+            var getElementByIndex= myList[1];
+            Console.WriteLine($"Element by index: {getElementByIndex}");
+
+            var getLength = myList.GetLength;
+            Console.WriteLine($"Collection Length: {getLength}");
+
+            PrintCollection(myList);
+        }
+
+        static void PrintCollection<T>(MyList<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine($"Name: {item}");
+            }
         }
     }
 }
