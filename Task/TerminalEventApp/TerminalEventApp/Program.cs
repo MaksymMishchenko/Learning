186b050e3b51a419e032c66eb7785e40d59fd14e
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-12 16:54:52 +0200

    Added finished program

--- Task/TerminalEventApp/TerminalEventApp/Program.cs
+++ Task/TerminalEventApp/TerminalEventApp/Program.cs
@@ -6,7 +6,22 @@ namespace TerminalEventApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create new object Terminal
+            var terminal = new Terminal(1000.00M);
+            Console.WriteLine("Денег на счету: ");
+            terminal.ShowBill();
+
+            //sign PutsMoney method to event
+            terminal.PutMoney += terminal.PutsMoney;
+            terminal.PutMoneyInvoke(100.00m);
+            Console.WriteLine("Денег на счету, после того, как положили: ");
+            terminal.ShowBill();
+
+            //sign WithdrawsMoney method to event
+            terminal.GetMoney += terminal.WithdrawsMoney;
+            terminal.GetMoneyInvoke(600.00m);
+            Console.WriteLine("Денег на счету, после того, как сняли: ");
+            terminal.ShowBill();
         }
     }
 }
