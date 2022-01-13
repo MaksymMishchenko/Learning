46ea5850c1776c9a88af098236b6739c63a84531
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-13 19:38:32 +0200

    Added finished program

--- Task/WeatherApp/WeatherApp/Program.cs
+++ Task/WeatherApp/WeatherApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace WeatherApp
 {
@@ -6,7 +7,32 @@ namespace WeatherApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // service
+            WeatherData weatherData = new WeatherData();
+
+            // users
+            User maks = new User("Макс");
+            User artur = new User("Артур");
+            User anton = new User("Антон");
+
+            // data
+            Weather berdychiv = new Weather("Бердичев", "-5 C", "Снег", "76%");
+            Weather kyiv = new Weather("Киев", "-8 C", "Снег", "90%");
+            Weather odessa = new Weather("Одесса", "+ 5 C", "Дождь", "95%");
+
+            // subscribe users
+            IDisposable arturSubscribe = weatherData.Subscribe(artur);
+            Thread.Sleep(500);
+            IDisposable antonSubscribe = weatherData.Subscribe(anton);
+            IDisposable maksSubscribe = weatherData.Subscribe(maks);
+
+            // notify users
+            weatherData.Notify(berdychiv);
+            weatherData.Notify(kyiv);
+
+            // unsubscribe user maks
+            maksSubscribe.Dispose();
+            weatherData.Notify(odessa);
         }
     }
 }
---
+++ Task/WeatherApp/WeatherApp/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WeatherApp
+{
+    class User: IObserver<Weather>
+    {
+        public string Name { get; set; }
+
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public void OnCompleted()
+        {
+            
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(error.Message);
+            Console.ResetColor();
+        }
+
+        public void OnNext(Weather value)
+        {
+            Console.WriteLine($"Подписчик: {Name}");
+            Console.WriteLine($"Город: {value.Town}");
+            Console.WriteLine($"Температура: {value.Temperature}");
+            Console.WriteLine($"Осадки: {value.Precipitation}");
+            Console.WriteLine($"Влажность: {value.Humidity}");
+            Console.WriteLine(new string('-', 15));
+            Console.WriteLine();
+        }
+    }
+}
