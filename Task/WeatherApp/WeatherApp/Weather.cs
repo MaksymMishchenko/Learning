f704226559b1bc105c37b228f3e00eee64f4df8d
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-13 18:26:12 +0200

    implement class Weather

---
+++ Task/WeatherApp/WeatherApp/Weather.cs
@@ -0,0 +1,18 @@
+﻿namespace WeatherApp
+{
+    class Weather
+    {
+        public string Town { get; }
+        public string Temperature { get; }
+        public string Precipitation { get; }
+        public string Humidity { get; }
+
+        public Weather(string town, string temperature, string precipitation, string humidity)
+        {
+            Town = town;
+            Temperature = temperature;
+            Precipitation = precipitation;
+            Humidity = humidity;
+        }
+    }
+}
