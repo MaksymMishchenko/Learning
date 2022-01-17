0f91923e5e745d2e1298ae4c41bc2e226d8cfa7d
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-17 23:34:02 +0200

    Added simple integration test

---
+++ Task/DeliveryServiceApi/DeliveryServiceApi.IntegrationTests/DeliveryControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Http;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+
+namespace DeliveryServiceApi.IntegrationTests
+{
+    [TestFixture]
+    public class DeliveryControllerTests
+    {
+        [Test]
+        public async Task CheckStatus_SendRequest_ShouldReturnOk()
+        {
+            // arrange
+            WebApplicationFactory<Startup> webHost = new WebApplicationFactory<Startup>().WithWebHostBuilder(_ => { });
+            HttpClient httpClient = webHost.CreateClient();
+
+            // actual
+
+            HttpResponseMessage response = await httpClient.GetAsync("api/delivery/check-status");
+
+            // assert
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ Task/DeliveryServiceApi/DeliveryServiceApi/Controllers/DeliveryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryServiceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeliveryController : Controller
+    {
+        [HttpGet("check-status")]
+        public IActionResult CheckStatus()
+        {
+            return Ok("Active");
+        }
+    }
+}
