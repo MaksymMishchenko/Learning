6129903f940281cde804ef328f1f951b912b279c
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-20 23:04:50 +0200

    Added program witch works without class ProxyChief

---
+++ Task/ProxyChiefApp/ProxyChiefApp/Chief.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ProxyChiefApp
+{
+    internal class Chief
+    {
+        public Dictionary<int, string> GetStatuses()
+        {
+            Dictionary<int, string> statuses = new Dictionary<int, string>();
+            statuses.Add(1, "ready");
+            statuses.Add(2, "not ready");
+            statuses.Add(3, "preparing");
+
+            Thread.Sleep(2000);
+
+            return statuses;
+        }
+
+        public List<Order> GetOrders()
+        {
+            List<Order> orders = new List<Order>
+            {
+                new Order() { Name = "Hot Dog", StatusId = RandomizeStatus()},
+                new Order(){Name = "Pizza", StatusId = RandomizeStatus()},
+                new Order(){Name = "Sandwich", StatusId = RandomizeStatus()}
+            };
+            return orders;
+        }
+
+        private static int RandomizeStatus() => new Random().Next(1, 4);
+    }
+}
--- Task/ProxyChiefApp/ProxyChiefApp/Program.cs
+++ Task/ProxyChiefApp/ProxyChiefApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace ProxyChiefApp
 {
@@ -6,7 +9,24 @@ namespace ProxyChiefApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Chief chief = new Chief();
+            while (true)
+            {
+                Thread.Sleep(2000);
+                Console.Clear();
+
+                Console.WriteLine("Welcome to Chief\n");
+                Console.WriteLine("========Orders========");
+
+                List<Order> orders = chief.GetOrders();
+
+                foreach (Order order in orders)
+                {
+                    string status = chief.GetStatuses().First(s => s.Key == order.StatusId).Value;
+
+                    Console.WriteLine($"Order: {order.Name}\t\t{status}");
+                }
+            }
         }
     }
 }
