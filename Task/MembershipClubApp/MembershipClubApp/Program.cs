27613108bbc3a3a9160b0b72e54d6d19bd696507
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-21 13:19:01 +0200

    Added empty project and readme file

---
+++ Task/MembershipClubApp/MembershipClubApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MembershipClubApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
