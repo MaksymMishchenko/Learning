742550feaa27d20b226d0cc8e0354c3ed22e823f
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-21 22:03:49 +0200

    Added finished program

--- WPFApplication/ToDoListApp/ToDoListApp/MainWindow.xaml.cs
+++ WPFApplication/ToDoListApp/ToDoListApp/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ToDoListApp.Models;
+using ToDoListApp.Services;
 
 namespace ToDoListApp
 {
@@ -20,9 +11,47 @@ namespace ToDoListApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        private BindingList<ToDoModel> _todoDataList;
+        private FileIOService _fileIoService;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIoService = new FileIOService(PATH);
+            try
+            {
+                _todoDataList = _fileIoService.LoadData();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                Close();
+            }
+
+            dgTodoList.ItemsSource = _todoDataList;
+            _todoDataList.ListChanged += _todoDataList_ListChanged;
+
+        }
+
+        private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIoService.SaveData(sender);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                    Close();
+                }
+            }
         }
     }
 }
