a35965006e13aa9ebec39c0bb21097812c40b4ea
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-21 14:19:56 +0200

    Added finished MembershipClubApp

--- Task/MembershipClubApp/MembershipClubApp/Program.cs
+++ Task/MembershipClubApp/MembershipClubApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MembershipClubApp.Factories;
+using MembershipClubApp.Membership;
+using System;
 
 namespace MembershipClubApp
 {
@@ -6,7 +8,32 @@ namespace MembershipClubApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(">>> Welcome to Membership Club <<<");
+            Console.WriteLine("Choose your membership you would like to buy?");
+
+            Console.WriteLine("'G' - Gym Membership");
+            Console.WriteLine("'P' - Gym Membership");
+            Console.WriteLine("'T' - Gym Membership");
+
+            string membershipTape = Console.ReadLine();
+
+            MembershipFactory factory = GetFactory(membershipTape);
+            IMembership membership = factory.GetMembership();
+
+            Console.WriteLine("\nMembership you've just created:\n");
+
+            Console.WriteLine($"\tName:\t\t{membership.Name}\n" +
+                              $"\tDescription:\t{membership.Description}\n" +
+                              $"\tPrice:\t\t{membership.GetPrice()}");
         }
+
+        public static MembershipFactory GetFactory(string membershipTape) =>
+            membershipTape.ToLower() switch
+            {
+                "g" => new GymMembershipFactory(100, "Basic mebership"),
+                "p" => new GymPlusPoolMembershipFactory(250, "Gym + Pool membership"),
+                "t" => new PersonalTrainingMembershipFactory(400, "Best for professionals"),
+                _ => null
+            };
     }
 }
