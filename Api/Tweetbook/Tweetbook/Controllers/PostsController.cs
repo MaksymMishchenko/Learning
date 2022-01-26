605fd65a19320c9165f6730ca609f118d2f9b44c
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-01-26 20:55:17 +0200

    Added method Create HttpPost-request

--- Api/Tweetbook/Tweetbook/Controllers/PostsController.cs
+++ Api/Tweetbook/Tweetbook/Controllers/PostsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Tweetbook.Contracts;
+using Tweetbook.Contracts.Requests;
+using Tweetbook.Contracts.Responses;
 using Tweetbook.Domain;
 
 namespace Tweetbook.Controllers
@@ -15,13 +17,30 @@ namespace Tweetbook.Controllers
             _posts = new List<Post>();
             for (var i = 0; i < 5; i++)
             {
-                _posts.Add(new Post{Id = Guid.NewGuid().ToString()});
+                _posts.Add(new Post { Id = Guid.NewGuid().ToString() });
             }
         }
         [HttpGet(ApiRoutes.Posts.GetAll)]
         public IActionResult GetAll()
         {
             return Ok(_posts);
+        }
+
+        [HttpPost(ApiRoutes.Posts.Create)]
+        public IActionResult Create([FromBody] CreatePostRequest postRequest)
+        {
+            var post = new Post { Id = postRequest.ToString() };
+            if (string.IsNullOrEmpty(post.Id))
+            {
+                post.Id = Guid.NewGuid().ToString();
+            }
+            _posts.Add(post);
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id);
+
+            var response = new PostResponse { Id = post.Id };
+
+            return Created(locationUri, response);
         }
     }
 }
