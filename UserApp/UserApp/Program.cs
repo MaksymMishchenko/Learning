9edfa1c5dea7d64585e22c8f33f986469189b914
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-02-05 19:11:50 +0200

    Install EntityFramework, established a connection, added users to database

--- UserApp/UserApp/Program.cs
+++ UserApp/UserApp/Program.cs
@@ -6,7 +6,18 @@ namespace UserApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (DbContent db = new DbContent())
+            {
+                User maks = new User { Name = "Maks", Age = 32 };
+            
+                User artur = new User { Name = "Artur", Age = 21 };
+            
+                User sveta = new User { Name = "Sveta", Age = 25 };
+            
+                db.Users.AddRange(maks, artur, sveta);
+                db.SaveChanges();
+                Console.WriteLine("Data added successfully!");
+            }
         }
     }
 }
