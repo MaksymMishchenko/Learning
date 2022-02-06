f56754c89d113ccfacf01201255bee8e7457c58f
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-02-06 09:40:34 +0200

    Added abstract methods to IUserData

--- UserApp/UserApp/Interfaces/IUserData.cs
+++ UserApp/UserApp/Interfaces/IUserData.cs
@@ -5,6 +5,8 @@ namespace UserApp.Interfaces
     interface IUserData
     {
         void AddsUsers();
+        void UpdateUsers();
+        void DeleteUsers();
         IEnumerable<User> GetUsers();
     }
 }
