01d899464e7ee3b80c372e264b27320856b041b2
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-02-17 12:02:04 +0200

    Implement lazy loading

---
+++ TractorApp/TractorApp/Models/Company.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TractorApp.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Tractor> Tractors { get; set; }
+    }
+}
--- TractorApp/TractorApp/Program.cs
+++ TractorApp/TractorApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using TractorApp.Models;
 
 namespace TractorApp
 {
@@ -6,7 +8,56 @@ namespace TractorApp
     {
         static void Main(string[] args)
         {
-           
+            using (var db = new DbContent())
+            {
+
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                var claas = new Company { Name = "Claas" };
+                var fendt = new Company { Name = "Fendt" };
+                var deutzFahr = new Company { Name = "Deutz Fahr" };
+                db.Company.AddRange(claas, fendt, deutzFahr);
+
+                var axion = new Tractor { Name = "AXION", Company = claas };
+                var arion = new Tractor { Name = "ARION", Company = claas };
+
+                var vario = new Tractor { Name = "VARIO 500", Company = fendt };
+                var nexys = new Tractor { Name = "Nexys 700", Company = fendt };
+
+                var nitro = new Tractor { Name = "Nitro", Company = deutzFahr };
+                var same = new Tractor { Name = "Same", Company = deutzFahr };
+                db.Tractor.AddRange(axion, arion, vario, nexys, nitro, same);
+
+                db.SaveChanges();
+            }
+
+            using (var db = new DbContent())
+            {
+                var tractorsList = db.Tractor.ToList();
+
+                foreach (var item in tractorsList)
+                {
+                    Console.WriteLine($"Tractor: {item.Name} | Company: {item.Company?.Name}");
+                }
+            }
+
+            Console.WriteLine(new string('-', 50));
+
+            using (var db = new DbContent())
+            {
+                var companies = db.Company.ToList();
+
+                foreach (var company in companies)
+                {
+                    Console.WriteLine($"Company: {company.Name}");
+
+                    foreach (var tractor in company.Tractors)
+                    {
+                        Console.WriteLine($"\tTractor: {tractor.Name}");
+                    }
+                }
+            }
         }
     }
 }
