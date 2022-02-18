715e1b6a1942494bcb2a179b92787de3689991d9
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-02-18 16:57:04 +0200

    Added new tasks to readme file

--- EmployeeApp/EmployeeApp/Model/Employee.cs
+++ EmployeeApp/EmployeeApp/Model/Employee.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+        //public string LastName { get; set; }
         public int Age { get; set; }
-        public bool IsMarried { get; set; }
+        //public bool IsMarried { get; set; }
         public Company Company { get; set; }
     }
 }
