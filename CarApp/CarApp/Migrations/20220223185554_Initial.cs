7be894b846c414393d7ca0a6540e4b87b1625fd7
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-02-23 21:43:47 +0200

    Added finished task

---
+++ CarApp/CarApp/Migrations/20220223185554_Initial.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarClassifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarClassifications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CarClassificationId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cars_CarClassifications_CarClassificationId",
+                        column: x => x.CarClassificationId,
+                        principalTable: "CarClassifications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_CarClassificationId",
+                table: "Cars",
+                column: "CarClassificationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "CarClassifications");
+        }
+    }
+}
---
+++ CarApp/CarApp/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace CarApp
+{
+    class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int? CarClassificationId { get; set; }
+        public CarClassification CarClassification { get; set; }
+    }
+}
