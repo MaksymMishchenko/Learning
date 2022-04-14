da7c63d3071915b085217e2f512845df9b724ec1
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-04-14 15:57:35 +0300

    Implement task

--- StockExchangeMonitorApp/StockExchangeMonitorApp/Program.cs
+++ StockExchangeMonitorApp/StockExchangeMonitorApp/Program.cs
@@ -6,7 +6,14 @@ namespace StockExchangeMonitorApp
     {
         static void Main(string[] args)
         {
-            
+            var result = new StockExchangeMonitor();
+            result.PriceChangeHandler = ShowPrice;
+            result.Start();
+        }
+
+        static void ShowPrice(int price)
+        {
+            Console.WriteLine($"New price is: {price}");
         }
     }
 }
