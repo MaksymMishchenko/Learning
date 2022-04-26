6572894baff6fe35a44ff6eaad66c54c31f7a6b5
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-04-26 14:48:47 +0300

    Added epmty project and readme file

---
+++ AsyncProgramming/WriteCharApp/WriteCharApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WriteCharApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
