9c929e3e9ec7d4d30685b86103c62e1286b28a96
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-06 16:04:45 +0300

    Added empty project and readme file

---
+++ L1HW1ADDT/PrintSymbolsApp/PrintSymbolsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrintSymbolsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
