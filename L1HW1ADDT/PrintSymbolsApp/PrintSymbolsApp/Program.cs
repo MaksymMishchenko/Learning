57f364e244a11e7ec4bcfc1f5d5bba8afa25f12e
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-06 16:27:48 +0300

    Added information about id thread

--- L1HW1ADDT/PrintSymbolsApp/PrintSymbolsApp/Program.cs
+++ L1HW1ADDT/PrintSymbolsApp/PrintSymbolsApp/Program.cs
@@ -7,6 +7,8 @@ namespace PrintSymbolsApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Method Main is started in thread id: {Thread.CurrentThread.ManagedThreadId}");
+
             ThreadPool.QueueUserWorkItem((WriteChar), '*');
 
             for (int i = 0; i < 160; i++)
@@ -14,10 +16,14 @@ namespace PrintSymbolsApp
                 Console.Write('#');
                 Thread.Sleep(100);
             }
+
+            Console.WriteLine($"Method Main is finished in thread id: {Thread.CurrentThread.ManagedThreadId}");
         }
 
         private static void WriteChar(object symbol)
         {
+            Console.WriteLine($"Method WriteChar is started in thread id: {Thread.CurrentThread.ManagedThreadId}.");
+
             char value = (char)symbol;
 
             for (int i = 0; i < 160; i++)
@@ -25,6 +31,8 @@ namespace PrintSymbolsApp
                 Console.Write(value);
                 Thread.Sleep(100);
             }
+
+            Console.WriteLine($"Method WriteChar is finished in thread id: {Thread.CurrentThread.ManagedThreadId}.");
         }
     }
 }
