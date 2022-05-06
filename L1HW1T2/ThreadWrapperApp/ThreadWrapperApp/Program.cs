a66879a402e68a54540aefa29fb27ccda601a525
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-06 18:13:34 +0300

    Added finished task

--- L1HW1T2/ThreadWrapperApp/ThreadWrapperApp/Program.cs
+++ L1HW1T2/ThreadWrapperApp/ThreadWrapperApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ThreadWrapperApp
 {
@@ -6,7 +7,35 @@ namespace ThreadWrapperApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Method Main is started in thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            var threadWorker = new ThreadWorker(WriteChar);
+            threadWorker.Start('*');
+
+            for (int i = 0; i < 160; i++)
+            {
+                Console.Write('#');
+                Thread.Sleep(100);
+            }
+
+            threadWorker.Wait();
+            Console.WriteLine($"Method Main is finished in thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            Console.ReadKey();
+        }
+
+        private static void WriteChar(object obj)
+        {
+            Console.WriteLine($"Method WriteChar is started in thread Id: {Thread.CurrentThread.ManagedThreadId}");
+            char value = (char)obj;
+
+            for (int i = 0; i < 160; i++)
+            {
+                Console.Write(value);
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine($"Method WriteChar is finished in thread Id: {Thread.CurrentThread.ManagedThreadId}");
         }
     }
 }
