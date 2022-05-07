59afb848628f5fab6a3b2ca25b6e7c8d5746d495
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-07 18:28:24 +0300

    Added empty project and readme file

---
+++ L2HW2T2/WriteSymbolApp/WriteSymbolApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WriteSymbolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
