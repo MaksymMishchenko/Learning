d43a009fb0fffb4a2a22a1d050fa4c37db98a5a8
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-07 18:53:43 +0300

    Added finished task

--- L2HW2T2/WriteSymbolApp/WriteSymbolApp/Program.cs
+++ L2HW2T2/WriteSymbolApp/WriteSymbolApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WriteSymbolApp
 {
@@ -6,7 +8,34 @@ namespace WriteSymbolApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Main is started in thread id: {Thread.CurrentThread.ManagedThreadId}");
+
+            Task task = new Task(WriteChar, '!');
+            task.Start();
+
+            while (!task.IsCompleted)
+            {
+                Console.Write('$');
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine($"Main is finished in thread id: {Thread.CurrentThread.ManagedThreadId}");
+            Console.ReadKey();
+        }
+
+        private static void WriteChar(object symbol)
+        {
+            Console.WriteLine($"WriteChar is started in thread id: {Thread.CurrentThread.ManagedThreadId}, Task id: {Task.CurrentId}");
+            char value = (char)symbol;
+
+            for (int i = 0; i < 160; i++)
+            {
+                Console.Write(value);
+                Thread.Sleep(200);
+            }
+
+            Console.Write('\n');
+            Console.WriteLine($"WriteChar is finished in thread id: {Thread.CurrentThread.ManagedThreadId}, Task id: {Task.CurrentId}");
         }
     }
 }
