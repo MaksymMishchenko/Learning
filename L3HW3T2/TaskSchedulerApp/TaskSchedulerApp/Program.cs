507511fe033945403b770bee45f5853b49262cf0
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-12 17:53:44 +0300

    Added empty project and readme file

---
+++ L3HW3T2/TaskSchedulerApp/TaskSchedulerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskSchedulerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello World!");
+        }
+    }
+}
