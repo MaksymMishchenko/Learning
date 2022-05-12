3dfcbc5c6cb0b63bc75fd69c300e5f5691d5a689
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-12 16:56:26 +0300

    Added finished task

--- L3HW3ADDT/DesktopFibonacciApp/DesktopFibonacciApp/MainWindow.xaml.cs
+++ L3HW3ADDT/DesktopFibonacciApp/DesktopFibonacciApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace DesktopFibonacciApp
 {
@@ -7,9 +9,38 @@ namespace DesktopFibonacciApp
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// Finds last Fibonacci number
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>Last number Fibonacci</returns>
+        private double FindLastFibonacciNumber(int number)
+        {
+            Func<int, double> fib = null;
+            fib = (x) => x > 1 ? fib(x - 1) + fib(x - 2) : x;
+            return fib.Invoke(number);
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            int str = int.Parse(txtLoop.Text);
+
+            TaskScheduler scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+            Task<double> task = new Task<double>(() => FindLastFibonacciNumber(str), TaskCreationOptions.LongRunning);
+            task.Start();
+            task.ContinueWith((t) =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    txtResult.Text += t.Result;
+                });
+            }, scheduler);
         }
     }
 }
