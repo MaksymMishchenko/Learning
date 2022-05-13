d456d16dae450abe69fa45e69100f7f988274088
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-13 16:35:51 +0300

    Added finished task

--- L3HW3T2/TaskSchedulerApp/TaskSchedulerApp/Program.cs
+++ L3HW3T2/TaskSchedulerApp/TaskSchedulerApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TaskSchedulerApp
 {
@@ -6,7 +8,20 @@ namespace TaskSchedulerApp
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Hello World!");
+            Console.WriteLine($"Method Main is started in Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+            StackTaskScheduler scheduler = new StackTaskScheduler();
+
+            for (int i = 0; i < 5; i++)
+            {
+                new Task(() =>
+                {
+                    Console.WriteLine($"Task {Task.CurrentId} is finished.");
+                    Thread.Sleep(1000);
+
+                }).Start(scheduler);
+            }
+
+            Console.ReadKey();
         }
     }
 }
