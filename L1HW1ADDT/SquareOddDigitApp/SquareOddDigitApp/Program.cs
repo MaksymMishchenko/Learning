1c11e785dafb016257719ae996e13b7c9dc24274
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-16 19:45:41 +0300

    Add empty project and readme file

---
+++ L1HW1ADDT/SquareOddDigitApp/SquareOddDigitApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SquareOddDigitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
