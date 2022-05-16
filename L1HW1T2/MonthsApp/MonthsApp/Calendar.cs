9e69080169595afb6aa79496af176d5e5062a701
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-16 20:40:04 +0300

    Add implements for class Calendar without methods by months and days

---
+++ L1HW1T2/MonthsApp/MonthsApp/Calendar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthsApp
+{
+    class Calendar : IEnumerable, IEnumerator
+    {
+        private readonly int[] months = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        private readonly int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        private int _position = -1;
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            if (_position < months.Length - 1)
+            {
+                _position++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            _position = -1;
+        }
+
+        public object Current => months[_position] + "-" + days[_position];
+    }
+}
--- L1HW1T2/MonthsApp/MonthsApp/Program.cs
+++ L1HW1T2/MonthsApp/MonthsApp/Program.cs
@@ -6,7 +6,40 @@ namespace MonthsApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Welcome to Calendar App");
+            Console.WriteLine(new string('-', 25));
+            Console.WriteLine("Make your choice:\n 1 - see data by months, \n 2 - see data by days, \n 3 - see all the information ");
+            var calendar = new Calendar();
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("You choose see data by months");
+                        break;
+                    }
+
+                case 2:
+                    {
+                        Console.WriteLine("You choose see data by days");
+                        break;
+                    }
+
+                case 3:
+                    {
+                        Console.WriteLine("You choose all the information by year");
+                        foreach (var item in calendar)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        break;
+                    }
+            }
+
+
         }
     }
 }
