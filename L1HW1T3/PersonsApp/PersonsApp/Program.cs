52024a19bc4c86fedff5b5934b4843de5599f921
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-17 12:41:56 +0300

    Add empty project and readme file

---
+++ L1HW1T3/PersonsApp/PersonsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
