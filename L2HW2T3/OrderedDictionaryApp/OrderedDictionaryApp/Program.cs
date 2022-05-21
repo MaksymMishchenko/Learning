01f54a3fe3b82480e345bfbef59d1b18a8264e04
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-21 22:04:41 +0300

    Add finished task

--- L2HW2T3/OrderedDictionaryApp/OrderedDictionaryApp/Program.cs
+++ L2HW2T3/OrderedDictionaryApp/OrderedDictionaryApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
 
 namespace OrderedDictionaryApp
 {
@@ -6,7 +8,31 @@ namespace OrderedDictionaryApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            var maks = new Sportsman { Key = 1, Name = "Maks", Hobbies = "Bicycle" };
+            var artur = new Sportsman { Key = 2, Name = "Artur", Hobbies = "Walk" };
+            var michael = new Sportsman { Key = 3, Name = "Michael", Hobbies = "Run" };
+            var peter = new Sportsman { Key = 3, Name = "Peter", Hobbies = "Swim" };
+
+            var dictionary = new OrderedDictionary(new KeyComparer());
+            dictionary.Add(maks, TrainingDay.Sunday);
+            dictionary.Add(artur, TrainingDay.Tuesday);
+            dictionary.Add(michael, TrainingDay.Wednesday);
+
+            try
+            {
+                dictionary.Add(peter, TrainingDay.Friday);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Такой ключ уже есть!");
+                Console.WriteLine(ex.Message);
+            }
+
+            foreach (DictionaryEntry item in dictionary)
+            {
+                Console.WriteLine(item.Key);
+                Console.WriteLine($"\tTraining day: {item.Value}");
+            }
         }
     }
 }
