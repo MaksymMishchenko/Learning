ec2fcbd16816b6f102ba55402db474d15ab6c682
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-21 17:58:21 +0300

    Add finished task

---
+++ L2HW2ADDT/MySortedCollectionApp/MySortedCollectionApp/InsensitiveComparerDescending.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace MySortedCollectionApp
+{
+    class InsensitiveComparerDescending : IComparer, IEqualityComparer
+    {
+        readonly CaseInsensitiveComparer comparer = new CaseInsensitiveComparer();
+        public int Compare(object x, object y)
+        {
+            return comparer.Compare(y, x);
+        }
+
+        public bool Equals(object x, object y)
+        {
+            return comparer.Compare(x, y) == 0;
+        }
+
+        public int GetHashCode(object obj)
+        {
+            return obj.ToString().ToLowerInvariant().GetHashCode();
+        }
+    }
+}
--- L2HW2ADDT/MySortedCollectionApp/MySortedCollectionApp/Program.cs
+++ L2HW2ADDT/MySortedCollectionApp/MySortedCollectionApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace MySortedCollectionApp
 {
@@ -6,7 +7,20 @@ namespace MySortedCollectionApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var sortedList = new SortedList();
+            var sortedList = new SortedList(new InsensitiveComparerDescending());
+
+            sortedList.Add("Maks", 32);
+            sortedList.Add("Artur", 22);
+            sortedList.Add("Dan", 45);
+            sortedList.Add("Nikolas", 25);
+
+            sortedList["NIKOLAS"] = 23;
+
+            foreach (DictionaryEntry item in sortedList)
+            {
+                Console.WriteLine($"Name: {item.Key}\t Age: {item.Value}");
+            }
         }
     }
 }
