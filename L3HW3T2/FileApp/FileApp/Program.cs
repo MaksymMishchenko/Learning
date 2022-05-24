957c3eda73610a036db519ac791582bb879cfd8b
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-24 09:32:28 +0300

    Add finished task

--- L3HW3T2/FileApp/FileApp/Program.cs
+++ L3HW3T2/FileApp/FileApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FileApp
 {
@@ -6,7 +7,25 @@ namespace FileApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"G:\SomeFile.txt";
+
+            var file = new FileInfo(path);
+            FileStream stream = file.Create();
+
+            var writer = new StreamWriter(stream);
+            writer.Write("It's necessary to do it!");
+            writer.Close();
+            stream.Close();
+
+            Console.WriteLine("File have been created!");
+
+            Console.WriteLine("Press Enter to read data from file.");
+            Console.ReadKey();
+
+            var reader = new StreamReader(path);
+            var text = reader.ReadToEnd();
+            Console.WriteLine($"Text in file: {text}");
+            reader.Close();
         }
     }
 }
