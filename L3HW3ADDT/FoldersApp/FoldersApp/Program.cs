159c313b18e65985f94c0e904de97a3dc23e2d6c
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-24 09:01:55 +0300

    Add finished task

--- L3HW3ADDT/FoldersApp/FoldersApp/Program.cs
+++ L3HW3ADDT/FoldersApp/FoldersApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FoldersApp
 {
@@ -6,7 +7,31 @@ namespace FoldersApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < 100; i++)
+            {
+                string path = @"G:\Folder_" + i;
+                var directory = new DirectoryInfo(path);
+                directory.Create();
+            }
+
+            Console.WriteLine("Folders have been created!");
+            Console.WriteLine("Click Enter to deletes created folders");
+
+            Console.ReadKey();
+
+            for (int i = 0; i < 100; i++)
+            {
+                string path = @"G:\Folder_" + i;
+                var directory = new DirectoryInfo(path);
+                
+                if (directory.Exists)
+                {
+                    Directory.Delete(path);
+                }
+            }
+
+            Console.WriteLine("Folders have been deleted!");
         }
     }
 }
