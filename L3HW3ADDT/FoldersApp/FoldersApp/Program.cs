9377f15b9c3575c8d7937cfee8addf3f8f61855b
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-24 08:43:07 +0300

    Add empty project and readme file

---
+++ L3HW3ADDT/FoldersApp/FoldersApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoldersApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
