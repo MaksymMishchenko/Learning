519d2bd2ab6d086df86ccc164804a6831eb4a07e
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-24 16:01:13 +0300

    Implement SearchFile Method

--- L3HW3T3/FileSeekerApp/Form1.Designer.cs
+++ L3HW3T3/FileSeekerApp/Form1.Designer.cs
@@ -30,6 +30,11 @@ namespace FileSeekerApp
         private void InitializeComponent()
         {
             this.checkedListBox = new System.Windows.Forms.CheckedListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // checkedListBox
@@ -41,22 +46,78 @@ namespace FileSeekerApp
             this.checkedListBox.TabIndex = 0;
             this.checkedListBox.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 159);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 279);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(191, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Review";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(411, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Archive";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(183, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 23);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Input name our file here";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.checkedListBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.CheckedListBox checkedListBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- L3HW3T3/FileSeekerApp/Form1.cs
+++ L3HW3T3/FileSeekerApp/Form1.cs
@@ -22,6 +22,58 @@ namespace FileSeekerApp
             }
         }
 
+        private string _file;
+
+        bool SearchFile(string path, string fileName)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            if (!dir.Exists)
+            {
+                return false;
+            }
+
+            FileInfo[] files = null;
+            try
+            {
+                files = dir.GetFiles(fileName);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            if (files.Length == 0)
+            {
+                DirectoryInfo[] directories = dir.GetDirectories();
+
+                if (directories.Length == 0)
+                {
+                    return false;
+                }
+
+                foreach (var item in directories)
+                {
+                    if (item.Attributes.Equals(FileAttributes.System | FileAttributes.Hidden | FileAttributes.Directory))
+                    {
+                        continue;
+                    }
+
+                    if (SearchFile(item.FullName, fileName))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            else
+            {
+                _file = files[0].FullName;
+                return true;
+            }
+        }
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -30,6 +82,26 @@ namespace FileSeekerApp
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox.Items.Count; i++)
+            {
+                if (checkedListBox.GetItemChecked(i))
+                {
+                    if (SearchFile(checkedListBox.Items[i].ToString(), textBox2.Text))
+                    {
+                        textBox1.Text = "File" + _file + " found"!;
+                        textBox1.Text = Environment.NewLine;
+                    }
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
