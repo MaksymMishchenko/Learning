674b2b056072008c61264158d14a57ee415e2655
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-24 09:18:00 +0300

    Add empty project and readme file

---
+++ L3HW3T2/FileApp/FileApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
