62ec14ffd22133a97a90ae119b8e34198c08d91e
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-26 20:35:30 +0300

    Add finished task

--- L4HW4ADDT/AuthenticationApp/AuthenticationApp/Program.cs
+++ L4HW4ADDT/AuthenticationApp/AuthenticationApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace AuthenticationApp
 {
@@ -6,7 +8,38 @@ namespace AuthenticationApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var list = new List<User>();
+
+            TryAgain:
+            Console.WriteLine("Welcome to our community. Please register.");
+            Console.WriteLine("Input your login: [use only a-z A-Z]\n");
+            string login = Console.ReadLine();
+
+            Console.WriteLine("Input your password: [use only a-z A-Z 0-9]\n");
+            string password = Console.ReadLine();
+
+            string loginPattern = @"^[a-zA-Z]+$";
+            string passPattern = @"^[a-zA-Z0-9]+$";
+
+            var checkLogin = new Regex(loginPattern);
+            var checkPass = new Regex(passPattern);
+
+
+            if (checkLogin.IsMatch(login) && checkPass.IsMatch(password))
+            {
+                list.Add(new User(login, password));
+                Console.WriteLine("Registration is successful!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input. Try again");
+                goto TryAgain;
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.Id}, {item.Login}, {item.Password}");
+            }
         }
     }
 }
---
+++ L4HW4ADDT/AuthenticationApp/AuthenticationApp/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AuthenticationApp
+{
+    internal class User
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public User(string login, string pass)
+        {
+            Login = login;
+            Password = pass;
+        }
+    }
+}
