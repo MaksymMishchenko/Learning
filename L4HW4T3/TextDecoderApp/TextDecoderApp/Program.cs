f9356ab7ce7019a9fef0fd39e05af2d1f632cf4e
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-30 19:17:37 +0300

    Add empty project and readme file

---
+++ L4HW4T3/TextDecoderApp/TextDecoderApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextDecoderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
