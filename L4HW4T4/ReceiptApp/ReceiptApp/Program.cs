4cf896717e7e18e01ad8ab2803fe92813162544c
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-30 20:37:29 +0300

    Add empty project and readme file

---
+++ L4HW4T4/ReceiptApp/ReceiptApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ReceiptApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
