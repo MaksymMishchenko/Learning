fc0f3d4df2b8c1654da60a62b25a32bc7ade80b1
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-30 20:50:42 +0300

    Add finished task

--- L4HW4T4/ReceiptApp/ReceiptApp/Program.cs
+++ L4HW4T4/ReceiptApp/ReceiptApp/Program.cs
@@ -10,7 +10,24 @@ namespace ReceiptApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var my = CultureInfo.CurrentCulture;
+            var us = new CultureInfo("en-Us");
+
+            var file = File.ReadAllText("product.txt", Encoding.UTF8);
+
+            Console.WriteLine(file);
+            Console.WriteLine(new string('-', 20));
+
+            var pattern = @"[0-9]+[\.\,][0-9]+";
+
+            var myCulture = Regex.Replace(file, pattern,
+                (m) => double.Parse(m.Value.Replace(".", ",")).ToString("C", my));
+            var usCulture = Regex.Replace(file, pattern,
+                (m) => double.Parse(m.Value.Replace(".", ",")).ToString("C", us));
+            Console.WriteLine(myCulture);
+            Console.WriteLine(new string('-', 20));
+
+            Console.WriteLine(usCulture);
         }
     }
 }
