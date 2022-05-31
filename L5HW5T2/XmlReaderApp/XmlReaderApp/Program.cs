59fd50928ee0b1b22665350988f748ba1f6c921e
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-31 18:29:17 +0300

    Add empty project and readme file

---
+++ L5HW5T2/XmlReaderApp/XmlReaderApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XmlReaderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
