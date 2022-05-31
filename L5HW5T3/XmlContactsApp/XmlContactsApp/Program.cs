e1fc2527f6877ac5b5eb9a49a1db3b1180ca3bfd
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-31 19:16:44 +0300

    Add empty project and readme file

---
+++ L5HW5T3/XmlContactsApp/XmlContactsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XmlContactsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
