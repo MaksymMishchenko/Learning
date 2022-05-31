deb5ea6d46420011c6c98280f784bd03bcf13d41
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-05-31 13:26:21 +0300

    Add finished task

--- L5HW5ADDT/XMLCreatorApp/XMLCreatorApp/Program.cs
+++ L5HW5ADDT/XMLCreatorApp/XMLCreatorApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Text;
+using System.Xml;
 
 namespace XMLCreatorApp
 {
@@ -6,7 +7,23 @@ namespace XMLCreatorApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var xmlTextWriter = new XmlTextWriter("TelephoneBook", Encoding.UTF8);
+
+            xmlTextWriter.Formatting = Formatting.Indented;
+            xmlTextWriter.IndentChar = '\t';
+            xmlTextWriter.Indentation = 1;
+
+            xmlTextWriter.WriteStartDocument();
+            xmlTextWriter.WriteStartElement("MyContacts");
+            xmlTextWriter.WriteStartElement("Contact");
+            xmlTextWriter.WriteStartAttribute("TelephoneNumber");
+            xmlTextWriter.WriteString("+380674153598");
+            xmlTextWriter.WriteEndAttribute();
+            xmlTextWriter.WriteString("Maksym Mischenko");
+            xmlTextWriter.WriteEndElement();
+            xmlTextWriter.WriteEndElement();
+
+            xmlTextWriter.Close();
         }
     }
 }
