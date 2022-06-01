921b86c9d3ce2d7d0741aaf79920e91d0980f27e
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-01 20:07:20 +0300

    Add finished task

--- L6HW6T3/TemperatureConverterApp/TemperatureConverterApp/Program.cs
+++ L6HW6T3/TemperatureConverterApp/TemperatureConverterApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 namespace TemperatureConverterApp
 {
@@ -6,7 +8,22 @@ namespace TemperatureConverterApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Assembly assembly = null;
+
+            try
+            {
+                assembly = Assembly.Load("ConverterLibrary");
+                Type type = assembly.GetType("ConverterLibrary.TemperatureConverter");
+
+                dynamic instance = Activator.CreateInstance(type);
+                Console.WriteLine($"From Celcium to Fahrenheit: {instance.ConvertToFahrenheit(10)}");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message); 
+            }
+
+            Console.ReadKey();
         }
     }
 }
