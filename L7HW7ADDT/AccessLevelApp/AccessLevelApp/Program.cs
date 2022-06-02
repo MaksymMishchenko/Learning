44be959688867908bba5e5b681a4c0c2736279d8
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-02 14:13:28 +0300

    Add finished task

--- L7HW7ADDT/AccessLevelApp/AccessLevelApp/Program.cs
+++ L7HW7ADDT/AccessLevelApp/AccessLevelApp/Program.cs
@@ -6,7 +6,46 @@ namespace AccessLevelApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var manager = new Manager();
+            var programmer = new Programmer();
+            var director = new Director();
+
+            Console.WriteLine("Employees works:");
+            Console.WriteLine(new string('-', 17));
+
+            manager.Work();
+            programmer.Work();
+            director.Work();
+
+            Type type = typeof(Manager);
+
+            object[] attributes = type.GetCustomAttributes(true);
+
+            Console.WriteLine("Employees name & Position:");
+            Console.WriteLine(new string('-', 27));
+
+            foreach (AccessLevelAttribute attribute in attributes)
+            {
+                Console.WriteLine($"Name: {attribute.Name},\t Position:\t {attribute.Position}");
+            }
+
+            type = typeof(Programmer);
+
+            attributes = type.GetCustomAttributes(true);
+
+            foreach (AccessLevelAttribute attribute in attributes)
+            {
+                Console.WriteLine($"Name: {attribute.Name},\t Position:\t {attribute.Position}");
+            }
+
+            type = typeof(Director);
+
+            attributes = type.GetCustomAttributes(true);
+
+            foreach (AccessLevelAttribute attribute in attributes)
+            {
+                Console.WriteLine($"Name: {attribute.Name},\t Position:\t {attribute.Position}");
+            }
         }
     }
 }
