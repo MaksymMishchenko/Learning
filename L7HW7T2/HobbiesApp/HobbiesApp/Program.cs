02c164a6b4bd8f5548e50fdb37077e7f60b8f01c
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-02 15:03:45 +0300

    Add finished task

--- L7HW7T2/HobbiesApp/HobbiesApp/Program.cs
+++ L7HW7T2/HobbiesApp/HobbiesApp/Program.cs
@@ -6,7 +6,13 @@ namespace HobbiesApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var hobbies = new Hobbies();
+            Console.WriteLine(hobbies.PlayBasketballOld());
+            Console.WriteLine(hobbies.PlayBasketballNew());
+            Console.WriteLine(hobbies.PlayFootballNew());
+
+            // [Obsolete("This method may be deleted in future version", true)] error = true;
+            //Console.WriteLine(hobbies.PlayFootballOld());
         }
     }
 }
