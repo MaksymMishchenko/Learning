31ef4b46cd813671bb8b3727e1e580141b61ac8e
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-02 14:53:42 +0300

    Add empty project and readme file

---
+++ L7HW7T2/HobbiesApp/HobbiesApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HobbiesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
