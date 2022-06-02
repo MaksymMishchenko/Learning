88087e1cb768321d7143111387168b542f340fb5
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-02 13:55:20 +0300

    Add custom AccessLevelAttribute, decorated classes Manager, Programmer, Director

---
+++ L7HW7ADDT/AccessLevelApp/AccessLevelApp/Programmer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AccessLevelApp
+{
+    [AccessLevel(AccessLevel.MediumLevel, Name = "Peter", Position = "Programmer")]
+    class Programmer : Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Programmer's work");
+        }
+    }
+}
