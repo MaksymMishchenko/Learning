cd84648148321a3ae0b56bd03c7987267fd5d01b
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-04 12:38:47 +0300

    Implement serialization to class Car without xml-attributes

---
+++ 8LHW8T2/CarApp/CarApp/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace CarApp
+{
+    public class Car
+    {
+        public string CarBrand { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public string MaxSpeed { get; set; }
+    }
+}
--- 8LHW8T2/CarApp/CarApp/Program.cs
+++ 8LHW8T2/CarApp/CarApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace CarApp
 {
@@ -6,7 +8,19 @@ namespace CarApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // var mercedes = new Car("Mercedes", "ML 500", 50.000M, "300");
+            var mercedes = new Car();
+            mercedes.CarBrand = "Mercedes";
+            mercedes.Model = "ML 500";
+            mercedes.Price = 50.000M;
+            mercedes.MaxSpeed = "300";
+            
+            FileStream stream = new FileStream("document.xml", FileMode.Create, FileAccess.Write);
+            XmlSerializer serializer = new XmlSerializer(typeof(Car));
+            serializer.Serialize(stream, mercedes);
+            stream.Close();
+            Console.WriteLine("Serialize was successful!");
+            Console.ReadKey();
         }
     }
 }
