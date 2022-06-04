aaf2dacf91a9c74b831a7af4351707cc5e1cdda3
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-04 11:17:16 +0300

    Add finished task

--- L8HW8ADDT/StudentsApp/StudentsApp/Program.cs
+++ L8HW8ADDT/StudentsApp/StudentsApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace StudentsApp
 {
@@ -6,7 +8,17 @@ namespace StudentsApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var artur = new Student(Guid.NewGuid(), "Artur", 23, "P-312");
+            artur.ShowInfo();
+
+            // serialization
+            using (var memoryStream = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(memoryStream, artur);
+                var bytes = memoryStream.ToArray();
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ L8HW8ADDT/StudentsApp/StudentsApp/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StudentsApp
+{
+    [Serializable]
+    class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Group { get; set; }
+
+        public Student(Guid id, string name, int age, string @group)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Group = @group;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Id: {Id}, Name: {Name}, Age: {Age}, Group: {Group}");
+        }
+    }
+}
