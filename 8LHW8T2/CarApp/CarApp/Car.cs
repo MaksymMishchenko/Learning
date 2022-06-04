18f50c95d567c9174b7871a0ab0ef685e173f4ad
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-04 12:47:10 +0300

    Implement serialization to class Car with xml-attributes

--- 8LHW8T2/CarApp/CarApp/Car.cs
+++ 8LHW8T2/CarApp/CarApp/Car.cs
@@ -1,10 +1,20 @@
-﻿namespace CarApp
+﻿using System.Xml.Serialization;
+
+namespace CarApp
 {
+    [XmlRoot("Type")]
     public class Car
     {
+        [XmlAttribute("Brand")]
         public string CarBrand { get; set; }
+
+        [XmlAttribute]
         public string Model { get; set; }
+
+        [XmlAttribute]
         public decimal Price { get; set; }
+
+        [XmlAttribute("Speed")]
         public string MaxSpeed { get; set; }
     }
 }
