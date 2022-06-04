e9f998960c8ae4460ddda1e46530a9716ff7f9dd
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-04 16:14:37 +0300

    Implement deserialization from file document.xml and display information to console

--- L8HW8T3/DeSerializationCarApp/DeSerializationCarApp/Program.cs
+++ L8HW8T3/DeSerializationCarApp/DeSerializationCarApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace DeSerializationCarApp
 {
@@ -6,7 +8,16 @@ namespace DeSerializationCarApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            XmlSerializer serializer = new XmlSerializer(typeof(Car));
+
+            FileStream original = new FileStream("document.xml", FileMode.Open, FileAccess.Read);
+
+            var auto = (Car)serializer.Deserialize(original);
+            Console.WriteLine($"Car: {auto.CarBrand}, Model: {auto.Model}, Price: {auto.Price}, MaxSpeed: {auto.MaxSpeed}");
+
+            Console.WriteLine("Deserialization was seccussful");
+
+            original.Close();
         }
     }
 }
