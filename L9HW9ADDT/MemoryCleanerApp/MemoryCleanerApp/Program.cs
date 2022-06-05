489479452bd923bcd2ff50388f003a5c6b8c68f4
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-05 16:41:08 +0300

    Implement interface IDisposable and Dispose pattern

--- L9HW9ADDT/MemoryCleanerApp/MemoryCleanerApp/Program.cs
+++ L9HW9ADDT/MemoryCleanerApp/MemoryCleanerApp/Program.cs
@@ -6,7 +6,22 @@ namespace MemoryCleanerApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Program is started");
+            Console.WriteLine(new string('-', 18));
+            using (var @object = new LargeObject())
+            {
+                for (int i = 0; i < 10000000; i++)
+                {
+                    @object.Add(new LargeObject());
+                }
+            }
+
+            var largeObject = new LargeObject();
+            largeObject.Dispose();
+            largeObject.Dispose();
+            largeObject.Dispose();
+
+            Console.ReadKey();
         }
     }
 }
