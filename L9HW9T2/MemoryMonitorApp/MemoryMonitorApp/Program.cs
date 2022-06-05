baa5965fce79ea47913475801df510b0a08165f1
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-05 17:58:25 +0300

    Implement class MemoryMonitor

--- L9HW9T2/MemoryMonitorApp/MemoryMonitorApp/Program.cs
+++ L9HW9T2/MemoryMonitorApp/MemoryMonitorApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace MemoryMonitorApp
 {
@@ -6,7 +7,18 @@ namespace MemoryMonitorApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var timer = new Timer(new MemoryMonitor(100000000).WarningMsg, "Memory limit is Exceeded!", 0, 200);
+
+            MyArray[] array = new MyArray[1500];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                new MyArray().ShowProgress(i);
+            }
+
+            timer.Dispose();
+
+            Console.ReadKey();
         }
     }
 }
