a769308d932b606dabe9d97f83dd1133bff8234a
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-05 17:42:52 +0300

    Add epmty project and readme file

---
+++ L9HW9T2/MemoryMonitorApp/MemoryMonitorApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemoryMonitorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
