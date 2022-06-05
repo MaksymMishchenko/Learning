3a1d4f70bac1c689b1e11335a8abf0faaa84827d
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-05 16:00:00 +0300

    Add empty project and readme file

---
+++ L9HW9ADDT/MemoryCleanerApp/MemoryCleanerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemoryCleanerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
