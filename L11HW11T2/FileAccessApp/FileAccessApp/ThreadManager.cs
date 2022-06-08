9e54862fa5b534d53f5a02b067d126b1ede45536
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-08 10:30:57 +0300

    Implement writing data from two files into the third file from several threads

---
+++ L11HW11T2/FileAccessApp/FileAccessApp/ThreadManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FileAccessApp
+{
+    internal class ThreadManager : IDisposable
+    {
+        private BlockObject _block;
+        public ThreadManager(BlockObject block)
+        {
+            _block = block;
+            _block.Enter();
+        }
+
+        public void Dispose()
+        {
+            _block.Exit();
+        }
+    }
+}
