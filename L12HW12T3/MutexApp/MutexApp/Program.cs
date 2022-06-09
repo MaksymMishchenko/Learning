68ad6ac9316624d75b5ce28d93e7b0cb2c42be83
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-09 13:44:17 +0300

    Add new task to readme file

---
+++ L12HW12T3/MutexApp/MutexApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MutexApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
