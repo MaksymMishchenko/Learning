7ad7a102cb6045446d46362fd85e4efa6f4d7cd5
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-09 13:50:01 +0300

    Implement class Mutex in class Program

--- L12HW12T3/MutexApp/MutexApp/Program.cs
+++ L12HW12T3/MutexApp/MutexApp/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Threading;
 
 namespace MutexApp
 {
     class Program
     {
+        static Mutex mutex = new Mutex(false, $"MyMytex-{new Guid()}");
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            mutex.WaitOne();
+
+            Console.WriteLine($"Method Enter to the safe zone");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Method Exit to the safe zone");
+
+            Console.ReadKey();
+
+            mutex.ReleaseMutex();
         }
     }
 }
