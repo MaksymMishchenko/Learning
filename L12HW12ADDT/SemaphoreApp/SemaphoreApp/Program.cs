49680f0fa4decc60e0d5249738fdabdd2a1ac885
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-09 12:30:57 +0300

    Implement Semaphore in task

--- L12HW12ADDT/SemaphoreApp/SemaphoreApp/Program.cs
+++ L12HW12ADDT/SemaphoreApp/SemaphoreApp/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
 
 namespace SemaphoreApp
 {
     class Program
     {
+        private static Semaphore pool = null;
+
+        public static void WriteLog(object name)
+        {
+            pool.WaitOne();
+            File.AppendAllText("log.txt", $"Method WriteLog was started in thread: {name}\n");
+            Thread.Sleep(500);
+            File.AppendAllText("log.txt", $"Method WriteLog was finished in thread: {name}\n");
+            pool.Release();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            pool = new Semaphore(1, 3);
+
+            for (int i = 0; i < 8; i++)
+            {
+                new Thread(WriteLog).Start(i);
+            }
         }
     }
 }
