7bdf4d73876769674e4fc5ebd7f6f27454f464b0
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-10 16:24:52 +0300

    Fill array random numbers then do parallel request and get odd numbers from array

--- L14HW14ADDT/OddNumbersApp/OddNumbersApp/Program.cs
+++ L14HW14ADDT/OddNumbersApp/OddNumbersApp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
 
 namespace OddNumbersApp
 {
@@ -6,7 +10,27 @@ namespace OddNumbersApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new int[1000000];
+
+            Random random = new Random();
+            Parallel.For(0, arr.Length, i => arr[i] = random.Next(-100, 100));
+
+            Thread.Sleep(1000);
+
+            ParallelQuery<int> oddNumbers = from item in arr.AsParallel()
+                                            where item % 2 != 0
+                                            orderby item
+                                            select item;
+            Thread.Sleep(1000);
+
+            Parallel.ForEach(oddNumbers, items =>
+            {
+                Console.Write($"{items} ");
+                Thread.Sleep(20);
+
+            });
+        
+            Console.ReadKey();
         }
     }
 }
