f0fb18f35a9cbe934e8889bfd24d12c918b64eab
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-10 17:06:45 +0300

    Run two methods asynchronously in TaskFactory context

--- L14HW14T2/ParallelTasksApp/ParallelTasksApp/Program.cs
+++ L14HW14T2/ParallelTasksApp/ParallelTasksApp/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ParallelTasksApp
 {
     class Program
     {
+        public static void WriteStars()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Thread.Sleep(200);
+                Console.Write("* ");
+            }
+        }
+        public static void WriteZero()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Thread.Sleep(200);
+                Console.Write("# ");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            TaskFactory factory = new TaskFactory();
+            factory.StartNew(() => Parallel.Invoke(WriteStars, WriteZero));
+
+            for (int i = 0; i < 20; i++)
+            {
+                Thread.Sleep(200);
+                Console.WriteLine("-");
+            }
+
+            Console.ReadKey();
         }
     }
 }
