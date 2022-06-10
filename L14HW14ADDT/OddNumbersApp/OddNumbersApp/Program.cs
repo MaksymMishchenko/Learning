06055ea6f154a1c2d39ea8f67cf0d40332d5f497
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-10 15:51:47 +0300

    Add empty project and readme file

---
+++ L14HW14ADDT/OddNumbersApp/OddNumbersApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OddNumbersApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
