0157541410c8e07c5af845b78be50f6742cf794c
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-13 16:25:45 +0300

    Implement method DoIncrement like async

--- L15HW15ADDT/IncrementTaskApp/IncrementTaskApp/Program.cs
+++ L15HW15ADDT/IncrementTaskApp/IncrementTaskApp/Program.cs
@@ -6,7 +6,15 @@ namespace IncrementTaskApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key no increment...");
+            Console.ReadKey();
+
+            for (int i = 0; i < 3; i++)
+            {
+                new Count().DoIncrementAsync();
+            }
+
+            Console.ReadKey();
         }
     }
 }
