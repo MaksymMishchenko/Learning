c9bbe32971c548b8855ab1ef97b130463ce0ceed
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-27 16:54:14 +0300

    Add array and display it to app.component.html

--- FirstAngularApp/src/app/app.component.ts
+++ FirstAngularApp/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export interface Post{
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'FirstAngularApp';
+  posts: Post[] = [
+    { title:'lorem1', text:'Lorem ipsum1', id: 1},
+    { title:'lorem2', text:'Lorem ipsum2', id: 2}
+  ]
 }
