2714c652975e1f951777eb13802cdd13c4b61765
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-28 13:29:01 +0300

    Add event to style directive

--- Directives/src/app/directives/style-directive.directive.ts
+++ Directives/src/app/directives/style-directive.directive.ts
@@ -1,11 +1,21 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appStyleDirective]'
 })
 export class StyleDirectiveDirective {
 
-  constructor(private el: ElementRef, private renderer: Renderer2)  {
-    this.renderer.setStyle(el.nativeElement, 'color','blue')
+  constructor(private el: ElementRef, private renderer: Renderer2) {
    }
+
+   @HostListener('click', ['$event.target']) onClick(event: Event) {
+    console.log(event)
+   }
+@HostListener('mouseenter') onEnter() {
+  this.renderer.setStyle(this.el.nativeElement, 'color','blue')
+}
+
+@HostListener('mouseleave') onLeave() {
+  this.renderer.setStyle(this.el.nativeElement, 'color', null)
+}
 }
