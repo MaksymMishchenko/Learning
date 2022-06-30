ff7ab5e75a3cf2b91adb990c2c212f64155825fa
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-06-30 12:27:51 +0300

    Implement log service

--- Directives/src/app/services/app-counter.service.ts
+++ Directives/src/app/services/app-counter.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from "@angular/core"
+import { LogService } from "./log.service"
 
 @Injectable({
     providedIn: 'root'
 })
 export class AppCounterService {
+
+    constructor(public logServive: LogService) { }
     counter = 0
     increase() {
+        this.logServive.log('Clicked increase button')
         this.counter++
     }
     decrease() {
+        this.logServive.log('Clicked decrease button')
         this.counter--
     }
 }
