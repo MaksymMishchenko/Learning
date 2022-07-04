f3af3b6cad37636d0ef71a50759f26831db9358e
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-07-04 13:36:22 +0300

    Implement forms controls, validations & users text helpers

---
+++ Forms/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  form: FormGroup
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+  }
+  submit(){
+    console.log('Form:', this.form)
+    const formData = {...this.form.value}
+    console.log('Form value: ', formData)
+  }
+}
