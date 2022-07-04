0f79b05b77439c478c6f97a5299d17865944c5b5
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-07-04 14:09:22 +0300

    Implement validation for country & city

--- Forms/src/app/app.component.ts
+++ Forms/src/app/app.component.ts
@@ -12,7 +12,11 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.form = new FormGroup({
       email: new FormControl('', [Validators.email, Validators.required]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      address: new FormGroup({
+        country: new FormControl('ua'),
+        city: new FormControl('Kyiv', Validators.required)
+      })
     })
   }
   submit(){
