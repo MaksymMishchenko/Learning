a48cc0868c871b79adfd8007582ada44129cc818
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-07-08 11:38:30 +0300

    Send headers in post method to server

--- httpClient/src/app/todo.service.ts
+++ httpClient/src/app/todo.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, delay, Observable, throwError } from "rxjs";
 
@@ -16,14 +16,18 @@ export class TodoService {
     constructor(private httpClient: HttpClient) { }
 
     addTodo(newTodo: Todo): Observable<Todo> {
-        return this.httpClient.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+        return this.httpClient.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo, {
+            headers: new HttpHeaders({
+                'MyCustomHeader': Math.random().toString()
+            })
+        })
     }
 
     fetchTodos(): Observable<Todo[]> {
         return this.httpClient.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
             .pipe(
                 delay(500),
-                catchError(error=>{
+                catchError(error => {
                     console.log('Error:', error.message)
                     return throwError(error)
                 }))
