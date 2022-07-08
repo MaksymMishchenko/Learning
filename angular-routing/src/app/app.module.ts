4bcffcb9b59d401095e207daea3e6101d080e273
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-07-08 17:56:41 +0300

    Implement module routing and add links to template

---
+++ angular-routing/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { AboutExtraComponent } from './about-extra/about-extra.component';
+import { HomeComponent } from './home/home.component';
+import { PostComponent } from './post/post.component';
+import { PostsComponent } from './posts/posts.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    AboutExtraComponent,
+    HomeComponent,
+    PostComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent, AboutComponent, AboutExtraComponent, HomeComponent, PostComponent, PostsComponent]
+})
+export class AppModule { }
