bbb030613c961a47684809523dd3c6079723a660
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-07-08 10:38:43 +0300

    Implement catch error in method catch Todos

--- httpClient/src/app/app.component.ts
+++ httpClient/src/app/app.component.ts
@@ -10,8 +10,11 @@ import { Todo, TodoService } from './todo.service';
 export class AppComponent implements OnInit {
 
   todos: Todo[] = []
-  todoTitle = ""
   loading = false
+  todoTitle = ''
+  error = ''
+
+  
 
   constructor(private todosService: TodoService) { }
 
@@ -36,11 +39,12 @@ export class AppComponent implements OnInit {
 
   fetchTodos() {
     this.loading = true
-    
-      this.todosService.fetchTodos()
+    this.todosService.fetchTodos()
       .subscribe(todos => {
         this.todos = todos
         this.loading = false
+      }, error => {
+        this.error = error.message
       })
   }
 
@@ -50,4 +54,10 @@ export class AppComponent implements OnInit {
     this.todos = this.todos.filter(t => t.id !== id)
   }
 
+  completeTodo(id: any) {
+    this.todosService.todoComplete(id)
+      .subscribe((todo) => {
+        this.todos.find(t => t.id === todo.id)!.completed = true
+      })
+  }
 }
--- httpClient/src/app/todo.service.ts
+++ httpClient/src/app/todo.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { delay, Observable } from "rxjs";
+import { catchError, delay, Observable, throwError } from "rxjs";
 
 export interface Todo {
     completed: boolean,
@@ -21,10 +21,20 @@ export class TodoService {
 
     fetchTodos(): Observable<Todo[]> {
         return this.httpClient.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
-        .pipe(delay(500))
+            .pipe(
+                delay(500),
+                catchError(error=>{
+                    console.log('Error:', error.message)
+                    return throwError(error)
+                }))
     }
 
-    removeTodos(id: number): Observable<void>{
+    removeTodos(id: number): Observable<void> {
         return this.httpClient.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    }
+    todoComplete(id: number): Observable<Todo> {
+        return this.httpClient.put<Todo>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+            completed: true
+        })
     }
 }
