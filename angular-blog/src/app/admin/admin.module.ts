7a418784bf0f09374a9b2ba4d8f39f1b7102ad09
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-07-13 11:30:44 +0300

    Add object User and check fields email & password

--- angular-blog/src/app/admin/admin.module.ts
+++ angular-blog/src/app/admin/admin.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { CreatePageComponent } from "./create-page/create-page.component";
 import { DashboardPageComponent } from "./dashboard-page/dashboard-page.component";
@@ -8,9 +9,16 @@ import { LoginPageComponent } from "./login-page/login-page.component";
 import { AdminLayoutComponent } from "./shared/components/admin-layout/admin-layout.component";
 
 @NgModule({
-    declarations: [AdminLayoutComponent],
+    declarations: [AdminLayoutComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    CreatePageComponent,
+    EditPageComponent
+    ],
     imports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
         RouterModule.forChild([
             {
                 path: '', component: AdminLayoutComponent, children: [
