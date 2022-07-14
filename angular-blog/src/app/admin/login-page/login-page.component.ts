db5e790177c0ba4a0e9e44774651a6b0b6c6fbaa
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-07-14 07:52:54 +0300

    Implement auth token

--- angular-blog/src/app/admin/login-page/login-page.component.ts
+++ angular-blog/src/app/admin/login-page/login-page.component.ts
@@ -11,6 +11,7 @@ import { AuthService } from '../shared/services/auth.services';
 })
 export class LoginPageComponent implements OnInit {
   form: FormGroup
+  submitted = false
 
   constructor(private auth: AuthService, private router: Router) { }
 
@@ -31,15 +32,17 @@ export class LoginPageComponent implements OnInit {
     if (this.form.invalid) {
       return
     }
+    this.submitted = true
 
     const user: User = {
       email: this.form.value.email,
-      password: this.form.value.password
+      password: this.form.value.password,
     }
 
     this.auth.login(user).subscribe(() => {
       this.form.reset()
       this.router.navigate(['/admin', 'dashboard'])
+      this.submitted = false
     })
   }
 }
--- angular-blog/src/app/admin/shared/services/auth.services.ts
+++ angular-blog/src/app/admin/shared/services/auth.services.ts
@@ -10,10 +10,16 @@ export class AuthService {
     constructor(private http: HttpClient) { }
 
     get token(): string {
-        return ''
+        const expDate = new Date(localStorage.getItem('fb-token-exp'))
+        if (new Date() > expDate) {
+            this.logout()
+            return null
+        }
+        return localStorage.getItem('fb-token')
     }
 
     login(user: User): Observable<any> {
+        user.returnSecureToken = true
         return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.apiKey}`, user)
             .pipe(
                 tap(this.setToken)
@@ -21,15 +27,20 @@ export class AuthService {
     }
 
     logout() {
-
+        this.setToken(null)
     }
 
     isAuthenticated(): boolean {
         return !!this.token
     }
 
-    private setToken(response: FbAuthResponse) {
-        console.log(response)
+    private setToken(response: FbAuthResponse | null) {
+        if (response) {
+            const expData = new Date(new Date().getTime() + +response.expiresIn * 1000)
+            localStorage.setItem('fb-token', response.idToken)
+            localStorage.setItem('fb-token-exp', expData.toString())
+        }else{
+            localStorage.clear()
+        }
     }
-
 }
