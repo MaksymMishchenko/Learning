20d7c147f8156f4e74e4f61c1cd998abe493b75e
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-07-15 22:16:45 +0300

    Get posts from database and show on dashboard page

--- angular-blog/src/app/admin/dashboard-page/dashboard-page.component.ts
+++ angular-blog/src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from 'src/app/shared/interfaces';
+import { PostsService } from 'src/app/shared/posts.service';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+  posts: Post[] = []
+  postSubscription: Subscription
 
-  constructor() { }
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.postSubscription = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.postSubscription) {
+      this.postSubscription.unsubscribe()
+    }
+  }
+
+  remove(id: string) {
+
   }
 
 }
--- angular-blog/src/app/shared/posts.service.ts
+++ angular-blog/src/app/shared/posts.service.ts
@@ -10,12 +10,24 @@ export class PostsService {
 
     create(post: Post): Observable<Post> {
         return this.http.post(`${environment.fbDbUrl}/posts.json`, post)
-        .pipe(map((response: FbCreateResponse) =>{
-           return {
-            ...post,
-            id: response.name,
-            date: new Date(post.date)
-           }
-        }))
+            .pipe(map((response: FbCreateResponse) => {
+                return {
+                    ...post,
+                    id: response.name,
+                    date: new Date(post.date)
+                }
+            }))
+    }
+
+    getAll(): Observable<Post[]> {
+        return this.http.get(`${environment.fbDbUrl}/posts.json`)
+            .pipe(map((response: { [key: string]: any }) => {
+                return Object.keys(response)
+                .map(key => ({
+                  ...response[key],
+                  id: key,
+                  date: new Date(response[key].date)  
+                }))
+            }))
     }
 }
