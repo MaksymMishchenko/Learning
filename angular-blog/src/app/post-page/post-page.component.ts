c980f66c30f0047fc5d84c9a941a97c4b310f37c
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-07-17 18:54:17 +0300

    Show posts on the main page

--- angular-blog/src/app/post-page/post-page.component.ts
+++ angular-blog/src/app/post-page/post-page.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from '../shared/interfaces';
 
 @Component({
   selector: 'app-post-page',
   templateUrl: './post-page.component.html',
   styleUrls: ['./post-page.component.scss']
 })
+
 export class PostPageComponent implements OnInit {
+
+  @Input() post : Post
 
   constructor() { }
 
