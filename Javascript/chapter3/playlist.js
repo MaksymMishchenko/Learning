7dfc225b44f13dbff9055de6ac3d1fe5253fcc87
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-08-05 19:39:50 +0300

    Add song to playlist

--- Javascript/chapter3/playlist.js
+++ Javascript/chapter3/playlist.js
@@ -1,12 +1,26 @@
 
-window.onload = init;
-
 function init() {
     var button = document.getElementById("addButton");
     button.onclick = heandleButtonClick;
 }
 
 function heandleButtonClick() {
-    alert("Button was clicked");
+
+    var textInput = document.getElementById("songTextInput");
+    var getSong = textInput.value;
+
+    if (getSong.trim() != "") {
+
+        var li = document.createElement("li");
+        li.innerHTML = getSong;
+        var ul = document.getElementById("playlist");
+        ul.appendChild(li);
+
+    }
+    else {
+        alert("Please, enter a song!");
+    }
 }
+
+window.onload = init;
 
