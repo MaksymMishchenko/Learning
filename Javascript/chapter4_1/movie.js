0b8b1dadc36757a24ef6b1bfb99fae2f400ae97c
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-08-09 18:01:16 +0300

    Add several objects Movie

--- Javascript/chapter4_1/movie.js
+++ Javascript/chapter4_1/movie.js
@@ -1,74 +1,35 @@
-var banzaiMovie = {
-	title: "Buckaroo Banzai",
-	genre: "Cult Classic",
-	rating: 5,
-	showtimes: ["1:00pm", "5:00pm", "7:00pm", "11:00pm"],
 
-	getNextShowing: function () {
+function Movie(title, genre, rating, showTimes) {
+	this.title = title;
+	this.genre = genre;
+	this.rating = rating;
+	this.showTimes = showTimes;
+	this.getNextShowing = function () {
 		var now = new Date().getTime();
-		for (var i = 0; i < this.showtimes.length; i++) {
+		for (var i = 0; i < this.showTimes.length; i++) {
 
-			var showtime = getTimeFromString(this.showtimes[i]);
+			var showtime = this.method(this.showTimes[i]);
 
 			if ((showtime - now) > 0) {
-				return "Next showing of " + this.title + " is " + this.showtimes[i];
+				return "Next showing of " + this.title + " is " + this.showTimes[i];
 			}
 		}
 		return null;
-	}
-};
-
-var movie1 = {
-	title: "Plan 9 from Outer Space",
-	genre: "Cult Classic",
-	rating: 2,
-	showtimes: ["3:00pm", "7:00pm", "11:00pm"],
-
-	getNextShowing: function () {
-		var now = new Date().getTime();
-		for (var i = 0; i < this.showtimes.length; i++) {
-
-			var showtime = getTimeFromString(this.showtimes[i]);
-
-			if ((showtime - now) > 0) {
-				return "Next showing of " + this.title + " is " + this.showtimes[i];
-			}
-		}
-		return null;
-	}
-};
-
-var movie2 = {
-	title: "Forbidden Planet",
-	genre: "Classic Sci-fi",
-	rating: 5,
-	showtimes: ["5:00pm", "9:00pm"],
-
-	getNextShowing: function () {
-		var now = new Date().getTime();
-		for (var i = 0; i < this.showtimes.length; i++) {
-
-			var showtime = getTimeFromString(this.showtimes[i]);
-
-			if ((showtime - now) > 0) {
-				return "Next showing of " + this.title + " is " + this.showtimes[i];
-			}
-		}
-		return null;
-	}
-};
-
-function getTimeFromString(str) {
-	var theTime = new Date();
-	var time = str.match(/(\d+)(?::(\d\d))?\s*(p?)/);
-	theTime.setHours(parseInt(time[1]) + (time[3] ? 12 : 0));
-	theTime.setMinutes(parseInt(time[2]) || 0);
-	return theTime.getTime();
+	};
+	this.method = function getTimeFromString(str) {
+		var theTime = new Date();
+		var time = str.match(/(\d+)(?::(\d\d))?\s*(p?)/);
+		theTime.setHours(parseInt(time[1]) + (time[3] ? 12 : 0));
+		theTime.setMinutes(parseInt(time[2]) || 0);
+		return theTime.getTime();
+	} 
 }
 
-var banzaiMovie = banzaiMovie.getNextShowing();
-alert(banzaiMovie);
-movie1 = movie1.getNextShowing();
-alert(movie1);
-movie2 = movie2.getNextShowing();
-alert(movie2);
+var banzaiMovie = new Movie("Buckaroo Banzai", "Cult Classic", 5, ["1:00pm", "5:00pm", "7:00pm", "11:00pm"]);
+alert(banzaiMovie.getNextShowing());
+
+var movie1 = new Movie("Plan 9 from Outer Space", "Plan 9 from Outer Space", 2, ["3:00pm", "7:00pm", "11:00pm"]);
+alert(movie1.getNextShowing());
+
+var movie2 = new Movie("Forbidden Planet", "Classic Sci-fi", 2, ["5:00pm", "9:00pm"]);
+alert(movie2.getNextShowing());
