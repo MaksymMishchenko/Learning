54796a46f02f8d20dc1c06e44c718474b3d7dab6
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-08-12 11:13:42 +0300

    Add method computeDistance whixh calculate distance between start & end coords

--- Javascript/chapter5/myLoc.js
+++ Javascript/chapter5/myLoc.js
@@ -1,3 +1,8 @@
+var ourCoords = {
+    latitude: 47.624851,
+    longitude: -122.52099
+};
+
 window.onload = getMyLocation;
 
 function getMyLocation() {
@@ -14,19 +19,19 @@ function showMyLocation(position) {
     var longitude = pos.longitude;
 
     var div = document.getElementById("location");
-    div.innerHTML = "Latitude: " + latitude + ", Longtitude: " + longitude;
+    div.innerHTML = "Latitude: " + latitude + ", Longitude: " + longitude;
+
+    var km = computeDistance(position.coords, ourCoords);
+    var distance = document.getElementById("distance");
+    distance.innerHTML = "You are " + km + " km grom the WickedlySmart HQ";
 }
 
 function displayError(error) {
 
-    errorType = {
-        0: "Unknown error",
-        1: "Permission denied by user",
-        2: "Position is not available",
-        3: "Request timed out"
-    };
+    errorsType = ["Unknown error", "Permission denied by user", "Position is not available", "Request timed out"];
 
-    var errorMessage = errorType[error.code];
+
+    var errorMessage = errorsType[error.code];
 
     if (error.code == 0 || error.code == 2) {
         errorMessage = errorMessage + " " + error.message;
@@ -34,5 +39,24 @@ function displayError(error) {
 
     var div = document.getElementById("location");
     div.innerHTML = errorMessage;
+}
 
+function computeDistance(startCoords, destCoords) {
+    var startLatRads = degreesToRadians(startCoords.latitude);
+    var startLongRads = degreesToRadians(startCoords.longitude);
+
+    var destLatRads = degreesToRadians(destCoords.latitude);
+    var destLongRads = degreesToRadians(destCoords.longitude);
+
+    var Radius = 6371; // Earth radius
+    var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) +
+        Math.cos(startLatRads) * Math.cos(destLatRads) *
+        Math.cos(startLongRads - destLongRads)) * Radius;
+
+    return distance;
+}
+
+function degreesToRadians(degrees) {
+    var radians = (degrees * Math.PI) / 180;
+    return radians;
 }
