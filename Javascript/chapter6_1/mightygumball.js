fdff057bdc70e1481e51738df75c7f645cc79caf
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-08-13 19:42:46 +0300

    Add JSONP data using script tag

--- Javascript/chapter6_1/mightygumball.js
+++ Javascript/chapter6_1/mightygumball.js
@@ -1,2 +1,14 @@
 window.onload = function () {
 }
+
+function updateSales(sales) {
+    var salesDiv = document.getElementById("sales");
+
+    for (let i = 0; i < sales.length; i++) {
+        var sale = sales[i];
+        var div = document.createElement("div");
+        div.setAttribute("class", "saleItem");
+        div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
+        salesDiv.appendChild(div);
+    }
+}
