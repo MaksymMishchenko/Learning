537e8c12a8c7da8d5641bc125144ebf7f674bf15
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-08-13 15:56:37 +0300

    Add new div tags to DOM-model & print sales in loop

--- Javascript/chapter6/mightygumball.js
+++ Javascript/chapter6/mightygumball.js
@@ -4,13 +4,22 @@ window.onload = function () {
     request.open("GET", url);
     request.onload = function () {
         if (request.status == 200) {
+            alert("Hello");
             updateSales(request.responseText);
         }
-    };
+    }
     request.send(null);
-};
 
-function updateSales(responseText){
-var div = document.getElementById("sales");
-div.innerHTML = responseText;
+    function updateSales(responseText) {
+        var salesDiv = document.getElementById("sales");
+        var sales = JSON.parse(responseText);
+
+        for (let i = 0; i < sales.length; i++) {
+            var sale = sales[i];
+            var div = document.createElement("div");
+            div.setAttribute("class", "saleItem");
+            div.innerHTML = sale.name + " sold " + sale.sales + "gumballs";
+            salesDiv.appendChild(div);
+        }
+    }
 }
