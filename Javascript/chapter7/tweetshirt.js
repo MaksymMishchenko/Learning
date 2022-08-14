b4b4b9aaf90c60f3f432aa78a1670331d5572f1b
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-08-14 22:06:26 +0300

    Add fillBackgroundColor method

--- Javascript/chapter7/tweetshirt.js
+++ Javascript/chapter7/tweetshirt.js
@@ -11,10 +11,17 @@ function previewHandler() {
     var selectObject = document.getElementById("shape");
     var index = selectObject.selectedIndex;
     var shape = selectObject[index].value;
+    fillBackgroundColor(canvas, context);
 
     if (shape == "square") {
         for (square = 0; square < 20; square++) {
             drawSquares(canvas, context);
+        }
+    }
+
+    if (shape == "circle") {
+        for (circle = 0; circle < 20; circle++) {
+            drawCircle(canvas, context);
         }
     }
 }
@@ -27,4 +34,12 @@ function drawSquares(canvas, context) {
 
     context.fillStyle = "lightblue";
     context.fillRect(x, y, w, w);
+}
+
+function fillBackgroundColor(canvas, context) {
+    var selectObject = document.getElementById("backgroundColor");
+    var index = selectObject.selectedIndex;
+    var backgroundColor = selectObject[index].value;
+    context.fillStyle = backgroundColor;
+    context.fillRect(0, 0, canvas.width, canvas.height);
 }
