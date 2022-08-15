692ef89aa59586ad769dd29a935f59b66ffc4b83
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-08-15 11:17:17 +0300

    Add smile cookie

--- Javascript/canvas/canvas.js
+++ Javascript/canvas/canvas.js
@@ -8,19 +8,38 @@ function handleCircle() {
     var context = canvas.getContext("2d");
 
     context.beginPath();
+    /*
     context.moveTo(50, 75);
     context.lineTo(125, 33);
     context.lineTo(170, 80);
     context.lineTo(140, 120);
     context.lineTo(110, 120);
-    context.closePath();
-   
-
-    context.lineWidth = 3;
+    //context.closePath();
+    */
+    context.arc(300, 100, 90, degreesToRadians(360), 0, true);
+    context.lineWidth = 5;
     context.stroke();
-    context.fillStyle = "red";
+    context.fillStyle = "yellow";
     context.fill();
 
-    
+    context.beginPath();
+    context.arc(250, 85, 25, degreesToRadians(360), 0, true);
+    context.stroke();
 
+    context.beginPath();
+    context.arc(350, 85, 25, degreesToRadians(360), 0, true);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(300, 105);
+    context.lineTo(300, 130);
+    context.stroke();
+
+    context.beginPath();
+    context.arc(300, 140, 35, degreesToRadians(180), 0, true );
+    context.stroke();
+}
+
+function degreesToRadians(degrees) {
+    return (degrees * Math.PI) / 180;
 }
