95d4909cf7f168b7031c532df054b59bcca5431f
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-09-09 10:55:21 +0300

    Implement remove sticky by key from localStorage, stikiesArray & DOM

--- Javascript/chapter9/notetoself.js
+++ Javascript/chapter9/notetoself.js
@@ -1,54 +1,74 @@
 window.onload = init;
 
 function init() {
-
     var button = document.getElementById("add_button");
     button.onclick = createSticky;
-
     var stickiesArray = getStickiesArray();
-
     for (var i = 0; i < stickiesArray.length; i++) {
         var key = stickiesArray[i];
         var value = localStorage[key];
-        addStickyToDOM(value);
+        addStickyToDOM(key, value);
     }
 }
 
-function addStickyToDOM(value) {
+function addStickyToDOM(key, value) {
     var stickies = document.getElementById("stickies");
     var sticky = document.createElement("li");
+    sticky.setAttribute("id", key);
     var span = document.createElement("span");
     span.setAttribute("class", "sticky");
     span.innerHTML = value;
     sticky.appendChild(span);
     stickies.appendChild(sticky);
+    sticky.onclick = deleteSticky;
 }
 
 function createSticky() {
-
     var stickiesArray = getStickiesArray();
-
     currentDate = new Date();
     var key = "sticky_" + currentDate.getTime();
-
     var value = document.getElementById("note_text").value;
     localStorage.setItem(key, value);
     stickiesArray.push(key);
     localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
-    
-    addStickyToDOM(value);
+
+    addStickyToDOM(key, value);
+
+    var input = document.getElementById("note_text");
+    input.value = "";
 }
 
 function getStickiesArray() {
-    var stickiesArray = localStorage["stickiesArray"];
-
+    var stickiesArray = localStorage.getItem("stickiesArray");
     if (!stickiesArray) {
         stickiesArray = [];
-        localStorage.setItem('stickiesArray', JSON.stringify(stickiesArray));
+        localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
     }
     else {
         stickiesArray = JSON.parse(stickiesArray);
     }
-
     return stickiesArray;
+}
+
+function deleteSticky(e) {
+    var key = e.target.id;
+    if (e.target.tagName.toLowerCase() == "span") {
+        key = e.target.parentNode.id;
+    }
+    localStorage.removeItem(key);
+    var stickiesArray = getStickiesArray();
+    if (stickiesArray) {
+        for (let index = 0; index < stickiesArray.length; index++) {
+            if (key == stickiesArray[index]) {
+                stickiesArray.splice(index, 1);
+            }
+        }
+    }
+    localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+    removeStickyFromDOM(key);
+}
+
+function removeStickyFromDOM(key) {
+    var sticky = document.getElementById(key);
+    sticky.parentNode.removeChild(sticky);
 }
