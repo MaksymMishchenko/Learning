185ac544c00a4e7ae8e255fdd04030bc2d365221
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-10-31 19:09:32 +0200

    fetch posts on ngOnInit test

--- angular-integration-tests/src/app/app.module.ts
+++ angular-integration-tests/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule
