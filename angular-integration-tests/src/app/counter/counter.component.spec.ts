eccf6c58afa33546b0ab5a0a7bcb60c6d6077200
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-10-31 17:07:52 +0200

    Renderer counter property test

--- angular-integration-tests/src/app/counter/counter.component.spec.ts
+++ angular-integration-tests/src/app/counter/counter.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { By } from "@angular/platform-browser"
+import { expand } from "rxjs"
 import { CounterComponent } from "./counter.component"
 
 describe('CounterComponent', () => {
@@ -17,7 +19,24 @@ describe('CounterComponent', () => {
   })
 
   it('should be created', () => {
-    // если компонент был создан, то в переменной component что-то есть
     expect(component).toBeDefined()
+  })
+
+  it('should render counter property', () => {
+    let num = 42
+    //меняем значение counter  
+    component.counter = num
+
+    // говорим ангуляру что мы присваиваем counter значение и применяем его
+    fixture.detectChanges()
+
+    //делаем запрос по шаблону
+    let de = fixture.debugElement.query(By.css('.counter'))
+
+    // в свойстве находится обычная джаваскрипт нода, которую мы можем проверять.
+    let el: HTMLElement = de.nativeElement
+
+    // передаем значение в строковом формате, потому что textContent свойство которое содержит строку
+    expect(el.textContent).toContain(num.toString())
   })
 })
