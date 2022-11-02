ccbf944f372b4a3e90e4410d95b31b9b116e12be
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-11-02 18:19:41 +0200

    Using authorization with service google firebase

--- travel-blog/src/app/admin/admin.module.ts
+++ travel-blog/src/app/admin/admin.module.ts
@@ -1,16 +1,24 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 import { AdminLayoutComponent } from "./components/admin-layout/admin-layout.component";
 import { LoginPageComponent } from "./login-page/login-page.component";
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
 
+import { AuthService } from "./shared/services/auth.service";
+import { SharedModule } from "../shared/shared.module";
+
 @NgModule({
     declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
     imports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        SharedModule,
         RouterModule.forChild([
             {
                 path: '', component: AdminLayoutComponent, children: [
@@ -23,7 +31,8 @@ import { EditPageComponent } from './edit-page/edit-page.component';
             }
         ])
     ],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [AuthService]
 })
 
 export class AdminModule {
---
+++ travel-blog/src/app/shared/components/interfaces.ts
@@ -0,0 +1,8 @@
+export class User {
+    email!: string
+    password!: string
+}
+
+export interface FbAuthResponse {
+    idToken: string
+}
