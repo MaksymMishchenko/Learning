fae6d77e70f7741772744b63db85f451300a4c9a
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-11-06 18:42:26 +0200

    Create & initialize create post page form, add quill editor

--- travel-blog/src/app/shared/components/interfaces.ts
+++ travel-blog/src/app/shared/components/interfaces.ts
@@ -1,10 +1,18 @@
-export class User {
-    email!: string
-    password!: string
+export interface User {
+    email: string
+    password: string
     returnSecureToken?: boolean
 }
 
 export interface FbAuthResponse {
     idToken: string,
     expiresIn: string
+}
+
+export interface Post {
+    id?: number
+    title: string
+    text: string,
+    author: string
+    date: Date
 }
