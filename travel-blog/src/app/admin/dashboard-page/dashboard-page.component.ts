fbd5215bb615c6107399b33de92a93fba944f995
Maksym Mishchenko <mishchenkomv@hotmail.com>
2022-11-08 14:14:36 +0200

    Get posts from firebase and show on dashboard page

--- travel-blog/src/app/admin/dashboard-page/dashboard-page.component.ts
+++ travel-blog/src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from 'src/app/shared/components/interfaces';
+import { PostsService } from 'src/app/shared/posts.service';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = []
+  pSub!: Subscription
+
+  constructor(public postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.pSub = this.postsService.getAllPosts().subscribe((post => {
+      this.posts = post
+    }))
+  }
+
+  remove(id: any) {
+
+  }
+
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
   }
 }
