aab2279b5951ef601e6daed5fc8bc510849f98ef
Maksym Mishchenko <mishchenkomv@hotmail.com>
2023-02-27 21:53:59 +0200

    Create comment component and render data to template

--- angular-comments/src/app/comments/comments.module.ts
+++ angular-comments/src/app/comments/comments.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { CommentComponent } from "./components/comment/comment.component";
 import { CommentsComponent } from './components/comments/comments.component';
 import { CommentsService } from "./services/comments.services";
 
 @NgModule({
   declarations: [
-    CommentsComponent
+    CommentsComponent,
+    CommentComponent
   ],
   imports:[CommonModule],
   exports: [CommentsComponent],
