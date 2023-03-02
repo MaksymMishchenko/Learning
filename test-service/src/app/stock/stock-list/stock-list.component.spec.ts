b484b8fe3165a4dd580cb4168ed140ba2f7a5635
Maksym Mishchenko <mishchenkomv@hotmail.com>
2023-03-02 21:08:59 +0200

    Testing StockListComponent with Fake Service

--- test-service/src/app/stock/stock-list/stock-list.component.spec.ts
+++ test-service/src/app/stock/stock-list/stock-list.component.spec.ts
@@ -3,35 +3,35 @@ import { StockService } from "src/app/services/stockService.service";
 import { StockItemComponent } from "../stock-item/stock-item.component";
 import { StockListComponent } from "./stock-list.component"
 
-describe('Test StockComponent with a Mock Service', () => {
+describe('Test StockList component with Fake Service', () => {
     let component: StockListComponent;
     let fixture: ComponentFixture<StockListComponent>
-    let stockService: StockService;
 
     beforeEach(waitForAsync(() => {
+        let stockServiceFake = {
+            getStocks: () => {
+                return [{ id: 0, name: 'Test', code: 'GFD', price: 50, previousPrice: 45, exchange: 'HGFEE', favorite: false }]
+            }
+        };
         TestBed.configureTestingModule({
-            declarations: [StockListComponent, StockItemComponent],
-            providers: [StockService]
-        });
+            declarations:[StockListComponent, StockItemComponent],
+            providers: [{
+                provide: StockService,
+                useValue: stockServiceFake
+            }]
+
+        }).compileComponents();
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(StockListComponent);
         component = fixture.componentInstance;
-        stockService = fixture.debugElement.injector.get(StockService);
-        let spy = spyOn(stockService, 'getStocks')
-            .and.returnValue([
-                {
-                    id: 1, name: 'Test', code: 'FDS', price: 52, previousPrice: 85, exchange: 'JHGFD', favorite: false
-                }
-            ]);
         fixture.detectChanges();
     });
 
-    it('should load stocks from mocked service on init'), () => {
+    it('should load stocks from fake service on init', () => {
         expect(component).toBeTruthy();
         expect(component.stocks.length).toEqual(1);
-        expect(component.stocks[0].code).toEqual('FDS');
-    };
-
-})
+        expect(component.stocks[0].code).toEqual('GFD');
+    });
+});
