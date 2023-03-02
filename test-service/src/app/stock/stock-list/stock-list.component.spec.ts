adeb04c5ffa592857876105045b3021782cad5a8
Maksym Mishchenko <mishchenkomv@hotmail.com>
2023-03-02 19:55:57 +0200

    Testing StockListComponent with Mock Service

--- test-service/src/app/stock/stock-list/stock-list.component.spec.ts
+++ test-service/src/app/stock/stock-list/stock-list.component.spec.ts
@@ -1,28 +1,37 @@
 import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { StockService } from "src/app/services/stockService.service";
 import { StockItemComponent } from "../stock-item/stock-item.component";
-import { StockListComponent } from "./stock-list.component";
+import { StockListComponent } from "./stock-list.component"
 
-
-describe('StockListComponent with real service', () => {
+describe('Test StockComponent with a Mock Service', () => {
     let component: StockListComponent;
-    let fixture: ComponentFixture<StockListComponent>;
+    let fixture: ComponentFixture<StockListComponent>
+    let stockService: StockService;
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [StockListComponent, StockItemComponent],
             providers: [StockService]
-        }).compileComponents();
+        });
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(StockListComponent);
         component = fixture.componentInstance;
+        stockService = fixture.debugElement.injector.get(StockService);
+        let spy = spyOn(stockService, 'getStocks')
+            .and.returnValue([
+                {
+                    id: 1, name: 'Test', code: 'FDS', price: 52, previousPrice: 85, exchange: 'JHGFD', favorite: false
+                }
+            ]);
         fixture.detectChanges();
-    })
-
-    it('should load stocks from real service on init', () => {
-        expect(component).toBeTruthy();
-        expect(component.stocks.length).toEqual(3);
     });
-});
+
+    it('should load stocks from mocked service on init'), () => {
+        expect(component).toBeTruthy();
+        expect(component.stocks.length).toEqual(1);
+        expect(component.stocks[0].code).toEqual('FDS');
+    };
+
+})
