3748c5748ab4f047288efba0308105092e883798
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-03-20 21:37:18 +0200

    Implement order model and view

---
+++ SportsStoreAPP/SportsStoreAPP/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStoreAPP.Models;
+
+namespace SportsStoreAPP.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout()
+        {
+            return View(new Order());
+        }
+    }
+}
