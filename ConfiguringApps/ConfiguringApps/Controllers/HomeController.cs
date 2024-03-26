982731be19e7ed59fb8a274eeb1e23bc92b1fdf4
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-03-26 17:56:41 +0200

    Add MVC and use default route

---
+++ ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View(new Dictionary<string, string>
+        {
+            ["message"] = "This is the index page"
+        });
+    }
+}
