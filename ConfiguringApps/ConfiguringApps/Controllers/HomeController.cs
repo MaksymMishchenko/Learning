c4a006951899dd1039e3f3e92a7339541592cee9
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-03-27 16:54:48 +0200

    create and set error method for production

--- ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
+++ ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -9,10 +9,21 @@ namespace ConfiguringApps.Controllers
 
         public HomeController(UptimeService up) => _uptime = up;
 
-        public IActionResult Index() => View(new Dictionary<string, string>
+        public IActionResult Index(bool throwException = false)
         {
-            ["message"] = "This is the index page",
-            ["Uptime"] = $"{_uptime.Uptime}ms"
-        });
+            if (throwException)
+            {
+                throw new NullReferenceException();
+            }
+
+            return View(new Dictionary<string, string>
+            {
+                ["message"] = "This is the index page",
+                ["Uptime"] = $"{_uptime.Uptime}ms"
+            });
+        }
+
+        public ViewResult Error() => View(nameof(Index),
+            new Dictionary<string, string> { ["message"] = "This is the error action" });
     }
 }
--- ConfiguringApps/ConfiguringApps/Program.cs
+++ ConfiguringApps/ConfiguringApps/Program.cs
@@ -1,5 +1,4 @@
 using ConfiguringApps.Infrastructures;
-using Microsoft.AspNetCore.Routing.Template;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseKestrel().UseContentRoot(Directory.GetCurrentDirectory())
@@ -8,21 +7,26 @@ builder.Services.AddSingleton<UptimeService>();
 builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
 
 var app = builder.Build();
-app.UseMiddleware<ErrorMiddleware>();
-app.UseMiddleware<BrowserTypeMiddleware>();
-app.UseMiddleware<ShortCircuitMIddleware>();
-app.UseMiddleware<ContentMiddleware>();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseStatusCodePages();
+}
+else {
+    app.UseExceptionHandler("/Home/Error");
+}
+
+//app.UseMiddleware<ErrorMiddleware>();
+//app.UseMiddleware<BrowserTypeMiddleware>();
+//app.UseMiddleware<ShortCircuitMIddleware>();
+//app.UseMiddleware<ContentMiddleware>();
+
 app.UseMvc(routes =>
 {
     routes.MapRoute(
         name: "deafult",
-        template: "{controller=Home}/{actin=Index}/{id?}"
+        template: "{controller=Home}/{action=Index}/{id?}"
         );
 });
 
-app.MapControllerRoute(
-    name: "Default",
-    pattern: "{controller=Home}/{actin=Index}/{id?}",
-    defaults: null
-    );
 app.Run();
