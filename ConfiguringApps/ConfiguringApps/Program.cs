5eeafb4a2064138883b9c88df2fc46b99324ce1f
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-03-27 19:54:11 +0200

    Configure access to appsettings.json file

--- ConfiguringApps/ConfiguringApps/Program.cs
+++ ConfiguringApps/ConfiguringApps/Program.cs
@@ -2,17 +2,36 @@ using ConfiguringApps.Infrastructures;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseKestrel().UseContentRoot(Directory.GetCurrentDirectory())
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        config.AddEnvironmentVariables();
+        if (args != null)
+        {
+            config.AddCommandLine(args);
+        }
+    })
 .UseIISIntegration();
+
+
 builder.Services.AddSingleton<UptimeService>();
 builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
 
 var app = builder.Build();
+
+if ((app.Configuration.GetSection("ShortCircuitMiddleware")?.GetValue<bool>("EnableBrowserShortCircuit")).Value)
+{
+    app.UseMiddleware<BrowserTypeMiddleware>();
+    app.UseMiddleware<ShortCircuitMIddleware>();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseStatusCodePages();
 }
-else {
+else
+{
     app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
