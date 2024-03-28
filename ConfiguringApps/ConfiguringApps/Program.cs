44be68aa7b9e5125f96dfa0be2859ce72d95ccad
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-03-28 21:12:58 +0200

    Create empty project

--- ConfiguringApps/ConfiguringApps/Program.cs
+++ ConfiguringApps/ConfiguringApps/Program.cs
@@ -1,53 +1,67 @@
 using ConfiguringApps.Infrastructures;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseKestrel().UseContentRoot(Directory.GetCurrentDirectory())
-    .ConfigureAppConfiguration((hostingContext, config) =>
+internal class Program
+{
+    private static void Main(string[] args)
     {
-        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-        config.AddEnvironmentVariables();
-        if (args != null)
+
+        var builder = WebApplication.CreateBuilder(args);
+        builder.WebHost.UseKestrel().UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                config.AddEnvironmentVariables();
+                if (args != null)
+                {
+                    config.AddCommandLine(args);
+                }
+            }).
+            ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+                logging.AddDebug();
+            })
+        .UseIISIntegration()
+        .UseDefaultServiceProvider((context, options) =>
         {
-            config.AddCommandLine(args);
+            options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+        });
+
+
+        builder.Services.AddSingleton<UptimeService>();
+        builder.Services.AddMvc(options => options.EnableEndpointRouting = false)
+            .AddMvcOptions(options => { options.RespectBrowserAcceptHeader = true; });
+
+        var app = builder.Build();
+
+
+        if ((app.Configuration.GetSection("ShortCircuitMiddleware")?.GetValue<bool>("EnableBrowserShortCircuit")).Value)
+        {
+            app.UseMiddleware<BrowserTypeMiddleware>();
+            app.UseMiddleware<ShortCircuitMIddleware>();
         }
-    }).
-    ConfigureLogging((hostingContext, logging) =>
-    {
-        logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-        logging.AddConsole();
-        logging.AddDebug();
-    })
-.UseIISIntegration();
 
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+        app.UseStaticFiles();
 
-builder.Services.AddSingleton<UptimeService>();
-builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
+        app.UseMvc(routes =>
+        {
+            routes.MapRoute(
+                name: "deafult",
+                template: "{controller=Home}/{action=Index}/{id?}"
+                );
+        });
 
-var app = builder.Build();
-
-if ((app.Configuration.GetSection("ShortCircuitMiddleware")?.GetValue<bool>("EnableBrowserShortCircuit")).Value)
-{
-    app.UseMiddleware<BrowserTypeMiddleware>();
-    app.UseMiddleware<ShortCircuitMIddleware>();
+        app.Run();
+    }
 }
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseStatusCodePages();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-}
-app.UseStaticFiles();
-
-app.UseMvc(routes =>
-{
-    routes.MapRoute(
-        name: "deafult",
-        template: "{controller=Home}/{action=Index}/{id?}"
-        );
-});
-
-app.Run();
