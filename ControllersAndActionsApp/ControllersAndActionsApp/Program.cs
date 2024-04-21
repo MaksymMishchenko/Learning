429879db528c9ce4c364b6bc1229fa9713c87f25
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-04-21 21:11:42 +0300

    Configure project

--- ControllersAndActionsApp/ControllersAndActionsApp/Program.cs
+++ ControllersAndActionsApp/ControllersAndActionsApp/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStatusCodePages();
+app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.UseSession();
+app.UseMvcWithDefaultRoute();
 
 app.Run();
