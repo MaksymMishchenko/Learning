fa38b116e42a21aae8321541833fd14529cc023a
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-05-26 21:08:11 +0300

    Get context data as action method parameters

---
+++ ControllersAndActionsApp/ControllersAndActionsApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersAndActionsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult SimpleForm() => View("SimpleForm");
+
+        public ViewResult ReceiveForm(string name, string city) {           
+
+            return View("Result", $"{name} lives in {city}");
+        }
+    }
+}
