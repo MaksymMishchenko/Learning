d4fa0f80ebb7749aaa24bc70f361bdaf28ea8ac9
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-07-02 16:18:46 +0300

    Creare ApiService which get data from API

---
+++ PillsTelegramBot/PillsTelegramBot/Api/ApiService.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using PillsTelegramBot.Models;
+
+namespace PillsTelegramBot
+{
+    internal class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<Pill>> GetPillsAsync(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string responseData = await response.Content.ReadAsStringAsync();
+                List<Pill> pills = JsonConvert.DeserializeObject<List<Pill>>(responseData);
+
+                return pills;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Request error: {e.Message}");
+                return null;
+            }
+        }
+
+        public async Task<Pill> GetPillAsync(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string responseData = await response.Content.ReadAsStringAsync();
+
+                Pill pill = JsonConvert.DeserializeObject<Pill>(responseData);
+                return pill;
+            }
+            catch (HttpRequestException e)
+            {
+
+                Console.WriteLine($"Request error: {e.Message}");
+                return null;
+            }
+        }
+    }
+}
--- PillsTelegramBot/PillsTelegramBot/Program.cs
+++ PillsTelegramBot/PillsTelegramBot/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace MyApp
+﻿using PillsTelegramBot;
+
+namespace MyApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var apiService = new ApiService();
+            var pills = apiService.GetPillsAsync("https://localhost:7036/api/pill");
+
+            foreach (var pill in pills.Result)
+            {
+                Console.WriteLine(pill.Name);
+                Console.WriteLine(pill.Category);
+                Console.WriteLine(pill.Manufacturer);
+            }
         }
     }
 }
