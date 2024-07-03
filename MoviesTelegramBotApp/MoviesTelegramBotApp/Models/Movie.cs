a34893bdbc4251d3a8f8990da2cf2b5248077ffe
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-07-03 15:01:59 +0300

    Implement AppDbContext, movie model & configure connection string

---
+++ MoviesTelegramBotApp/MoviesTelegramBotApp/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace MoviesTelegramBotApp.Models
+{
+    internal class Movie
+    {
+        public int MovieId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Country { get; set; }
+        public decimal Budget { get; set; }
+    }
+}
