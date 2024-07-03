b600b35e26f5b740202e21aff3105789837a043c
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-07-03 15:13:12 +0300

    Implement DatabaseProvider which gets connection string from appsettings.json

---
+++ MoviesTelegramBotApp/MoviesTelegramBotApp/Database/DatabaseProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MoviesTelegramBotApp.Database
+{
+    internal class DatabaseProvider
+    {
+        public DatabaseProvider()
+        {
+            SetUpProvider();
+        }
+
+        private void SetUpProvider()
+        {
+            var serviceProvider = new ServiceCollection().AddDbContext<ApplicationDbContext>(options =>
+            options.UseMySQL(GetConnectionString())).BuildServiceProvider();
+        }
+
+        private string GetConnectionString()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            return configuration.GetConnectionString("DefaultString");
+        }
+    }
+}
--- MoviesTelegramBotApp/MoviesTelegramBotApp/Program.cs
+++ MoviesTelegramBotApp/MoviesTelegramBotApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MoviesTelegramBotApp.Database;
+
+namespace MoviesTekegramBotApp
+{
+    internal class Program {
+        public static void Main()
+        {
+            new DatabaseProvider();
+        }
+    }
+}
