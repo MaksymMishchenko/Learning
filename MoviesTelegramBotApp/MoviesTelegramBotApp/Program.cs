942d086e5e0276d54f06a8c6b2a1c6b7cb367a7a
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-07-05 22:39:34 +0300

    SOLID principles refactoring code

--- MoviesTelegramBotApp/MoviesTelegramBotApp/Program.cs
+++ MoviesTelegramBotApp/MoviesTelegramBotApp/Program.cs
@@ -1,12 +1,12 @@
-﻿using MoviesTelegramBotApp.Database;
-using MoviesTelegramBotApp.Models;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MoviesTelegramBotApp.Database;
+using MoviesTelegramBotApp.Services;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
-using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.ReplyMarkups;
 
 namespace MoviesTelegramBotApp
 {
@@ -14,79 +14,37 @@ namespace MoviesTelegramBotApp
     {
         private static TelegramBotClient _botClient;
         private static CancellationTokenSource _cts;
+
         public static void Main()
         {
             _botClient = new TelegramBotClient("7075613694:AAE14JDh8bVT6AaOJkFT9_dc8qiowSlEGmw");
             _cts = new CancellationTokenSource();
 
-            var receiverOptions = new ReceiverOptions
+            var movieService = new MovieService(new ServiceCollection().AddDbContext<ApplicationDbContext>(options =>
+            options.UseMySQL(GetConnectionString())).BuildServiceProvider().GetRequiredService<ApplicationDbContext>());
+
+            var botService = new BotService(_botClient);
+            var updateHandler = new UpdateHandler(botService, movieService);
+
+            var recieverOptions = new ReceiverOptions
             {
                 AllowedUpdates = Array.Empty<UpdateType>()
             };
 
-            _botClient.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOptions, cancellationToken: _cts.Token);
+            _botClient.StartReceiving(updateHandler.HandleUpdateAsync, HandleErrorAsync, recieverOptions, _cts.Token);
 
-            Console.WriteLine("Bot is up and running...");
+            Console.WriteLine("Bot is up and running");
             Console.ReadLine();
         }
 
-        private static async Task HandleUpdateAsync(ITelegramBotClient bot, Update update, CancellationToken cts)
+        private static string GetConnectionString()
         {
-            if (update.Type == UpdateType.Message && update.Message!.Type == MessageType.Text)
-            {
-                var chatId = update.Message.Chat.Id;
-                var messageText = update.Message;
-                var me = await _botClient.GetMeAsync();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
 
-                var startKeyBoard = new ReplyKeyboardMarkup(new KeyboardButton("Start")) { ResizeKeyboard = true };
-
-                if (messageText?.Text?.ToLower() == "/start")
-                {
-                    var response = $"<strong>Lights, Camera, Action!</strong>\nWelcome to <em><strong>{me.FirstName}</strong></em>, the hottest Telegram hangout for movie lovers! Whether you're a die-hard cinephile or just enjoy a good popcorn flick, this is your spot to discuss all things film.";
-                    await _botClient.SendTextMessageAsync(chatId, response, parseMode: ParseMode.Html);
-                    await SendMenuAsync(chatId, cts);
-                }
-                else
-                {
-                    await HandleMenuResposeAsync(bot, messageText!, cts);
-                }
-            }
-        }
-
-        private static async Task SendMenuAsync(long chatId, CancellationToken cts)
-        {
-            var replyKeyBoardMarkup = new ReplyKeyboardMarkup(new[] {
-
-            new KeyboardButton[] { "Movies", "Cartoons"}
-            })
-            {
-                ResizeKeyboard = true
-            };
-
-            await _botClient.SendTextMessageAsync(
-                chatId,
-                text: "Choose an option",
-                replyMarkup: replyKeyBoardMarkup,
-                cancellationToken: cts);
-        }
-
-        private static async Task HandleMenuResposeAsync(ITelegramBotClient bot, Message message, CancellationToken cts)
-        {
-            switch (message.Text)
-            {
-                case "Movies":
-
-                    await SendMoviesAsync(bot, message.Chat.Id, cts);
-                    break;
-
-                case "Cartoons":
-                    //todo: functionality in progress
-                    break;
-
-                default:
-                    await _botClient.SendTextMessageAsync(message.Chat.Id, "The command is not recognized");
-                    break;
-            }
+            return configuration.GetConnectionString("DefaultConnection");
         }
 
         private static Task HandleErrorAsync(ITelegramBotClient bot, Exception ex, CancellationToken cts)
@@ -99,64 +57,6 @@ namespace MoviesTelegramBotApp
 
             Console.WriteLine(errorMessage);
             return Task.CompletedTask;
-        }
-
-        private static List<Movie> GetMovies()
-        {
-            return new DatabaseService().GetApplicationDbContext().Movies.ToList();
-        }
-
-        private static string BuildMoviesResponse()
-        {
-            List<Movie> movies = GetMovies();
-            var response = new StringBuilder();
-
-            if (!movies.Any())
-                return "No movies found";
-
-            foreach (var movie in movies)
-            {
-                response.AppendLine($"Name: {movie.Name}");
-                response.AppendLine($"Genre: {movie.Category} ");
-                response.AppendLine($"Description: {movie.Description}");
-                response.AppendLine($"Country: {movie.Country} ");
-                response.AppendLine($"Budget: {movie.Budget}");
-                response.AppendLine($"ImageUrl: {movie.ImageUrl}");
-                response.AppendLine($"Trailer: {movie.MovieUrl}");
-                response.AppendLine(new string('-', 60));
-                response.AppendLine();
-            }
-
-            return response.ToString();
-        }
-
-        private static async Task SendMoviesAsync(ITelegramBotClient bot, long chatId, CancellationToken cts)
-        {
-            List<Movie> movies = GetMovies();
-            string response = BuildMoviesResponse();
-
-            foreach (var movie in movies)
-            {
-                if (!string.IsNullOrEmpty(movie.ImageUrl))
-                {
-                    await _botClient.SendPhotoAsync(
-                    chatId: chatId,
-                        photo: new Telegram.Bot.Types.InputFiles.InputOnlineFile(movie.ImageUrl),
-                        parseMode: ParseMode.Html,
-                        caption: $"<strong>Name:</strong> {movie.Name}\n<strong>Genre:</strong> {movie.Category}\n<strong>Description:</strong> {movie.Description}\n<strong>Country:</strong> {movie.Country}\n<strong>Budget:</strong> {movie.Budget}\n<strong>Trailer:</strong> {movie.MovieUrl}",
-                        cancellationToken: cts
-                        );
-                }
-
-                else
-                {
-                    await _botClient.SendTextMessageAsync(
-                        chatId,
-                        text: response,
-                        cancellationToken: cts
-                    );
-                }
-            }
         }
     }
 }
