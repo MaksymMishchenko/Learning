a544fdd91660d435ff39a6ebfea7c6c9e0387b65
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-08-11 16:08:18 +0300

    Create empty project for test global exception handling

---
+++ GlobalExceptionHandling/GlobalExceptionHandling/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace GlobalExceptionHandling
+{
+    internal class Program
+    {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello");
+        }
+    }
+}
