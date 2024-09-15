0a453dffd781b42a65e4b6de9f8beae42831cbd5
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-09-15 11:50:32 +0300

    Implement Friend class

---
+++ GenericPhoneList/PhoneListApp/PhoneBook/Friend.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneListApp.PhoneBook
+{
+    internal class Friend : PhoneNumber
+    {
+        public Friend(string name, string number, bool isWn) : base(name, number) { }
+
+        public bool IsWorkNumber { get; private set; }
+    }
+}
