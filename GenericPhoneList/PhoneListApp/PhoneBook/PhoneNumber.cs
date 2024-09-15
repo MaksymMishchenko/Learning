aeed257bf003d935d9918e595c4a88a596b68afc
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-09-15 11:47:45 +0300

    Implement PhoneNumber class

---
+++ GenericPhoneList/PhoneListApp/PhoneBook/PhoneNumber.cs
@@ -0,0 +1,13 @@
+﻿namespace PhoneListApp.PhoneBook
+{
+    internal class PhoneNumber
+    {
+        public PhoneNumber(string number, string name)
+        {
+            Name = name;
+            Number = number;
+        }
+        public string Name { get; set; }
+        public string Number { get; set; }
+    }
+}
