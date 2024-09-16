994076a164a4342809c12bdac6c7ca8e55d21d9d
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-09-16 22:34:03 +0300

    Implement generic delegate with refers to specific methods

--- GenericDelegateApp/Program.cs
+++ GenericDelegateApp/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GenericDelegateApp
+{
+    delegate T SomeOp<T>(T t);
+
+    internal class GenDelegateDemo
+    {
+        static int Sum(int number)
+        {
+            int result = 0;
+            for (int i = number; i > 0; i--)
+            {
+                result += i;
+            }
+
+            return result;
+        }
+
+        static string Reflection(string str)
+        {
+            string result = "";
+
+            foreach (var ch in str)
+            {
+                result = ch + result;
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            SomeOp<int> delInt = Sum;
+            Console.WriteLine(delInt(5));
+
+            Console.WriteLine();
+
+            SomeOp<string> delStr = Reflection;
+            Console.WriteLine(delStr("Hello"));
+        }
+    }
+}
