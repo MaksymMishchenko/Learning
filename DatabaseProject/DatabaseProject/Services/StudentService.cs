f46169c57e7a83645dc2c358461db7c7daee4dfa
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-11-11 12:31:22 +0200

    Implement Student service with connection to database

--- DatabaseProject/DatabaseProject/Services/StudentService.cs
+++ DatabaseProject/DatabaseProject/Services/StudentService.cs
@@ -1,32 +1,50 @@
-﻿using DatabaseProject.Models;
+﻿using DatabaseProject.DbContexts;
+using DatabaseProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseProject.Services
 {
     public class StudentService : IStudentService
     {
-        public Task<bool> AddStudentAsync(Student newStudent)
+        private readonly SchoolDbContext _context;
+        public StudentService(SchoolDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+        public async Task<bool> AddStudentAsync(Student newStudent)
+        {
+            _context.Students.Add(newStudent);
+            var result = await _context.SaveChangesAsync();
+            return result > 0;
         }
 
-        public Task<bool> DeleteStudentAsync(int id)
+        public async Task<bool> DeleteStudentAsync(int id)
         {
-            throw new NotImplementedException();
+            var student = await _context.Students.FindAsync(id);
+            if (student is null)
+            {
+                return false;
+            }
+            _context.Students.Remove(student);
+            var result = await _context.SaveChangesAsync();
+            return result > 0;
         }
 
-        public Task<bool> EditStudentAsync(Student editStudent)
+        public async Task<bool> EditStudentAsync(Student editStudent)
         {
-            throw new NotImplementedException();
+            _context.Update(editStudent);
+            var result = await _context.SaveChangesAsync();
+            return result > 0;
         }
 
-        public Task<Student> GetStudentAsync(int id)
+        public async Task<Student> GetStudentAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Students.FindAsync(id);
         }
 
-        public Task<List<Student>> GetStudentsAsync()
+        public async Task<List<Student>> GetStudentsAsync()
         {
-            throw new NotImplementedException();
+            return await _context.Students.ToListAsync();
         }
     }
 }
