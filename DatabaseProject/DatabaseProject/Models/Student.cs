079672e463dce6f6963f1a3fdeb15eebeafb61fe
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-11-11 11:32:54 +0200

    Set string properties in Student model as nullable

--- DatabaseProject/DatabaseProject/Models/Student.cs
+++ DatabaseProject/DatabaseProject/Models/Student.cs
@@ -3,9 +3,9 @@
     public class Student
     {
         public int StudentId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public DateTime BirthDay { get; set; }
-        public string Address { get; set; }
+        public string? Address { get; set; }
     }
 }
