048f8cf60acc4ee7772389f12d1f262737368bed
MaksymMishchenko <mishchenkomv@hotmail.com>
2024-11-11 10:43:38 +0200

    Implement IStudentService interface and add to DI container

--- DatabaseProject/DatabaseProject/Program.cs
+++ DatabaseProject/DatabaseProject/Program.cs
@@ -1,6 +1,9 @@
+using DatabaseProject.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<IStudentService, StudentService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ DatabaseProject/DatabaseProject/Services/StudentService.cs
@@ -0,0 +1,32 @@
+﻿using DatabaseProject.Models;
+
+namespace DatabaseProject.Services
+{
+    public class StudentService : IStudentService
+    {
+        public Task<bool> AddStudentAsync(Student newStudent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteStudentAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EditStudentAsync(Student editStudent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Student> GetStudentAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Student>> GetStudentsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
